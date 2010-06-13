a99b4944e46996641bd2c112e4ee78754d29c7fe
chesteric31 <chesteric31@users.noreply.github.com>
2010-06-13 09:14:15 +0000

    Adding implements PersonDao interface + extends of GenericDaoImpl. Adding contructor. Remove all methods from the GenericDao and entityManager.

--- JSAMS/src/be/jsams/server/dao/impl/PersonDaoImpl.java
+++ JSAMS/src/be/jsams/server/dao/impl/PersonDaoImpl.java
@@ -2,8 +2,6 @@ package be.jsams.server.dao.impl;
 
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 
 import org.apache.commons.logging.Log;
@@ -12,18 +10,20 @@ import org.apache.commons.logging.LogFactory;
 import be.jsams.server.dao.PersonDao;
 import be.jsams.server.model.Person;
 
-public class PersonDaoImpl implements PersonDao {
+public class PersonDaoImpl extends GenericDaoImpl<Person> implements PersonDao {
 
 	private final Log LOGGER = LogFactory.getLog(this.getClass());
 
-	@PersistenceContext
-	private EntityManager entityManager;
+	public PersonDaoImpl(Class<Person> type) {
+		super(type);
+	}
 
-	@Override
-	public List<Person> findAll() {
+	public List<Person> findByName(String name) {
 		List<Person> persons = null;
 		try {
-			Query query = entityManager.createQuery("SELECT p FROM Person p");
+			Query query = getEntityManager().createQuery(
+					"SELECT p FROM Person p WHERE p.name = :name");
+			query.setParameter("name", name);
 			persons = query.getResultList();
 		} catch (RuntimeException re) {
 			LOGGER.error(re);
@@ -31,64 +31,4 @@ public class PersonDaoImpl implements PersonDao {
 		return persons;
 	}
 
-	@Override
-	public Person findById(Long id) {
-		Person person = null;
-		try {
-			person = entityManager.find(Person.class, id);
-		} catch (RuntimeException re) {
-			LOGGER.error(re);
-		}
-		return person;
-	}
-
-	@Override
-	public Person findByName(String name) {
-		Person person = null;
-		try {
-			Query query = entityManager
-					.createQuery("SELECT p FROM Person p WHERE p.name = :name");
-			query.setParameter("name", name);
-			person = (Person) query.getSingleResult();
-		} catch (RuntimeException re) {
-			LOGGER.error(re);
-		}
-		return person;
-	}
-
-	@Override
-	public Person merge(Person person) {
-		Person result = null;
-		try {
-			result = entityManager.merge(person);
-		} catch (RuntimeException re) {
-			LOGGER.error(re);
-		}
-		return result;
-	}
-
-	@Override
-	public void persist(Person person) {
-		LOGGER.debug("Persisting Person instance");
-		try {
-			entityManager.persist(person);
-			LOGGER.debug("Persist successful");
-		} catch (RuntimeException re) {
-			LOGGER.error("Persist failed", re);
-		}
-	}
-
-	@Override
-	public void remove(Person person) {
-		try {
-			entityManager.remove(person);
-		} catch (RuntimeException re) {
-			LOGGER.error(re);
-		}
-	}
-
-	@Override
-	public void remove(Long id) {
-		this.remove(this.findById(id));
-	}
 }
