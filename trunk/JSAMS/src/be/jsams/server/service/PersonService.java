8168cc9f6185fe2e5d7af928b7521b5c016c9b18
chesteric31 <chesteric31@a5fa9106-e037-4f10-9215-0b9fab2b6631>
2010-06-13 09:16:35 +0000

    Replace persist by add, merge by update. Correction findByName return: replace by List of Person.

--- trunk/JSAMS/src/be/jsams/server/service/PersonService.java
+++ trunk/JSAMS/src/be/jsams/server/service/PersonService.java
@@ -6,17 +6,17 @@ import be.jsams.server.model.Person;
 
 public interface PersonService {
 
-	public void persist(Person person);
+	public void add(Person person);
 
 	public void remove(Person person);
 
 	public void remove(Long id);
 
-	public Person merge(Person person);
+	public void update(Person person);
 
 	public Person findById(Long id);
 
-	public Person findByName(String name);
+	public List<Person> findByName(String name);
 
 	public List<Person> findAll();
 
