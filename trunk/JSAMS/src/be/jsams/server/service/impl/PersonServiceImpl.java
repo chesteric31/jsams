74d2b27174a0d5cbab905d61b6ab0f00ea1c5878
chesteric31 <chesteric31@a5fa9106-e037-4f10-9215-0b9fab2b6631>
2010-06-23 13:24:45 +0000

    Suppress the annotations @Override over the methods.

--- trunk/JSAMS/src/be/jsams/server/service/impl/PersonServiceImpl.java
+++ trunk/JSAMS/src/be/jsams/server/service/impl/PersonServiceImpl.java
@@ -18,37 +18,30 @@ public class PersonServiceImpl implements PersonService {
 		this.personDao = personDao;
 	}
 
-	@Override
 	public List<Person> findAll() {
 		return personDao.findAll();
 	}
 
-	@Override
 	public Person findById(Long id) {
 		return personDao.findById(id);
 	}
 
-	@Override
 	public List<Person> findByName(String name) {
 		return personDao.findByName(name);
 	}
 
-	@Override
 	public void update(Person person) {
 		personDao.update(person);
 	}
 
-	@Override
 	public void add(Person person) {
 		personDao.add(person);
 	}
 
-	@Override
 	public void remove(Person person) {
 		personDao.remove(person);
 	}
 
-	@Override
 	public void remove(Long id) {
 		personDao.remove(id);
 	}
