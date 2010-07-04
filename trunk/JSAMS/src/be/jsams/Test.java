d272bdca139ec868102668679a7d833a669c77c6
chesteric31 <chesteric31@a5fa9106-e037-4f10-9215-0b9fab2b6631>
2010-07-04 18:31:50 +0000

    Adding pdf testings.

--- trunk/JSAMS/src/be/jsams/Test.java
+++ trunk/JSAMS/src/be/jsams/Test.java
@@ -1,30 +1,39 @@
 package be.jsams;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.text.ParseException;
-import java.util.List;
 
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import be.jsams.pdf.PdfService;
+import be.jsams.pdf.impl.PdfServiceImpl;
 
-import be.jsams.server.model.Person;
-import be.jsams.server.service.PersonService;
+import com.lowagie.text.Document;
+import com.lowagie.text.DocumentException;
+import com.lowagie.text.Phrase;
+import com.lowagie.text.pdf.PdfPCell;
+import com.lowagie.text.pdf.PdfPTable;
+import com.lowagie.text.pdf.PdfWriter;
 
 public class Test {
 
-	public static void main(String[] args) throws ParseException {
-		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
-				"ApplicationContext.xml");
-		PersonService personService = (PersonService) context
-				.getBean("personService");
+	public static void main(String[] args) throws ParseException,
+			DocumentException, IOException {
+		// ClassPathXmlApplicationContext context = new
+		// ClassPathXmlApplicationContext(
+		// "ApplicationContext.xml");
+		// PersonService personService = (PersonService) context
+		// .getBean("personService");
 		// Person newPerson = new Person();
 		// newPerson.setName("BAUER");
 		// Timestamp timestamp = new Timestamp(new Date().getTime());
 		// newPerson.setCreationDate(timestamp);
 		// personService.add(newPerson);
-		List<Person> persons = personService.findByName("BAUER");
-
-		for (Person person : persons) {
-			personService.remove(person);
-		}
+		// List<Person> persons = personService.findByName("BAUER");
+		//
+		// for (Person person : persons) {
+		// personService.remove(person);
+		// }
 
 		// - Chargement et compilation du rapport
 		// try {
@@ -46,6 +55,21 @@ public class Test {
 		// // TODO Auto-generated catch block
 		// e.printStackTrace();
 		// }
+
+		PdfService pdf = new PdfServiceImpl();
+		Document document = pdf.createDocument();
+		PdfWriter.getInstance(document, new FileOutputStream("file.pdf"));
+		document.open();
+		PdfPTable table = pdf.createTable(2);
+		PdfPCell cell = pdf.createCell(new Phrase("value"));
+		PdfPCell cell2 = pdf.createCell(new Phrase("value2"));
+		table.addCell(cell);
+		table.addCell(cell2);
+		document.add(table);
+		document.close();
+
+		Runtime.getRuntime().exec(
+				"rundll32 url.dll,FileProtocolHandler file.pdf");
 
 	}
 }
