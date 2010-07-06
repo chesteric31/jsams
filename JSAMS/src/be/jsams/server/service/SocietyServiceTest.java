386f306caf13e309628432263ab717d8b028c554
chesteric31 <chesteric31@users.noreply.github.com>
2010-07-06 10:07:55 +0000

    Update junits.

--- JSAMS/src/be/jsams/server/service/SocietyServiceTest.java
+++ JSAMS/src/be/jsams/server/service/SocietyServiceTest.java
@@ -3,6 +3,7 @@ package be.jsams.server.service;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import org.junit.Before;
@@ -14,6 +15,8 @@ import org.springframework.test.context.junit4.AbstractTransactionalJUnit4Spring
 import org.springframework.test.context.transaction.TransactionConfiguration;
 import org.springframework.transaction.annotation.Transactional;
 
+import be.jsams.server.model.Address;
+import be.jsams.server.model.ContactInformation;
 import be.jsams.server.model.LegalForm;
 import be.jsams.server.model.Society;
 
@@ -26,44 +29,66 @@ public class SocietyServiceTest extends
 	@Autowired
 	private SocietyService societyService;
 
-	Society newSociety = null;
+	private Society newSociety = null;
 
 	@Before
 	public void setUp() throws Exception {
 		newSociety = new Society();
 		newSociety.setLabel("LABEL");
+
+		LegalForm form = new LegalForm();
+		form.setLabel("Form");
+		societyService.createLegalForm(form);
+		newSociety.setLegalForm(form);
+		
+		Address societyAddress = new Address();
+		societyAddress.setCity("Brussels");
+		societyAddress.setCountry("Belgium");
+		societyAddress.setNumber("1B");
+		societyAddress.setStreet("Rue Neuve");
+		societyAddress.setZipCode(1000);
+		newSociety.setAddress(societyAddress);
+		
+		ContactInformation societyContactInformation = new ContactInformation();
+		societyContactInformation.setEmail("x.x@x.com");
+		societyContactInformation.setGsm("+32499555444");
+		societyContactInformation.setPhone("+322123456");
+		newSociety.setContactInformation(societyContactInformation);
+		
+		String activity = "Activity";
+		newSociety.setActivity(activity);
+		newSociety.setCapital(new BigDecimal(100.00D));
+		newSociety.setResponsible("Binard Eric");
 	}
 
 	@Test
 	public void testPersist() {
-		LegalForm form = new LegalForm();
-		form.setLabel("Form");
-		
-		societyService.add(newSociety);
-		assertNotNull(societyService.findAll());
+		societyService.create(newSociety);
+		List<Society> societies = societyService.findAll();
+		assertNotNull(societies);
 	}
 
 	@Test
 	@Rollback(value = false)
 	public void testRemove() {
-		societyService.add(newSociety);
-		societyService.remove(newSociety);
+		societyService.create(newSociety);
+		societyService.delete(newSociety);
 		List<Society> societies = societyService.findAll();
 		assertTrue(societies == null || societies.isEmpty());
 	}
 
 	@Test
 	public void testUpdate() {
-//		societyService.add(newSociety);
-//		newSociety.setLabel("Labellll");
-//		societyService.update(newSociety);
-//		List<Person> persons = societyService.findByName("NOM");
-//		assertTrue(persons != null && !persons.isEmpty());
+		societyService.create(newSociety);
+		newSociety.setLabel("Updated Label");
+		societyService.update(newSociety);
+		Society foundSociety = societyService.findById(newSociety.getId());
+		assertNotNull(foundSociety);
 	}
 
 	@Test
 	public void testFindById() {
-		societyService.add(newSociety);
+		societyService.create(newSociety);
 		Long id = newSociety.getId();
 		Society society = societyService.findById(id);
 		assertNotNull(society);
@@ -71,7 +96,7 @@ public class SocietyServiceTest extends
 
 	@Test
 	public void testFindAll() {
-		societyService.add(newSociety);
+		societyService.create(newSociety);
 		List<Society> societies = societyService.findAll();
 		assertTrue(societies != null && !societies.isEmpty());
 	}
