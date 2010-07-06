14855e56889aa74ac700028747719a3b690a6aef
chesteric31 <chesteric31@users.noreply.github.com>
2010-07-06 10:05:49 +0000

    Remove extension for GenericService Adding methods: create, delete, findAll, findById and update.

--- JSAMS/src/be/jsams/server/service/PersonService.java
+++ JSAMS/src/be/jsams/server/service/PersonService.java
@@ -4,8 +4,20 @@ import java.util.List;
 
 import be.jsams.server.model.Person;
 
-public interface PersonService extends GenericService<Person> {
+public interface PersonService {
 
+	public void create(Person person);
+
+	public void delete(Person person);
+
+	public void delete(Long id);
+
+	public void update(Person person);
+
+	public Person findById(Long id);
+
+	public List<Person> findAll();
+	
 	public List<Person> findByName(String name);
 
 }
