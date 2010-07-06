322c9246051c042340019c0bf9e9d79620943fd0
chesteric31 <chesteric31@a5fa9106-e037-4f10-9215-0b9fab2b6631>
2010-07-06 10:07:01 +0000

    Remove extension for GenericService Adding methods: create, delete, findAll, findById, update and createLegalForm.

--- trunk/JSAMS/src/be/jsams/server/service/SocietyService.java
+++ trunk/JSAMS/src/be/jsams/server/service/SocietyService.java
@@ -1,7 +1,24 @@
 package be.jsams.server.service;
 
+import java.util.List;
+
+import be.jsams.server.model.LegalForm;
 import be.jsams.server.model.Society;
 
-public interface SocietyService extends GenericService<Society> {
+public interface SocietyService {
+
+	public void create(Society society);
+
+	public void delete(Society society);
+
+	public void delete(Long id);
+
+	public void update(Society society);
+
+	public Society findById(Long id);
+
+	public List<Society> findAll();
+	
+	public void createLegalForm(LegalForm legalForm);
 
 }
