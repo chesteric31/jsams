52a2c0f7290b02ac0a7101cb564e6447ef0a056c
chesteric31 <chesteric31@users.noreply.github.com>
2010-07-06 10:00:48 +0000

    Suppress the annotations @Override over the methods.

--- JSAMS/src/be/jsams/pdf/impl/PdfServiceImpl.java
+++ JSAMS/src/be/jsams/pdf/impl/PdfServiceImpl.java
@@ -11,31 +11,26 @@ import com.lowagie.text.pdf.PdfPTable;
 
 public class PdfServiceImpl implements PdfService {
 
-	@Override
 	public PdfPCell createCell(final Phrase value) {
 		PdfPCell cell = new PdfPCell(value);
 		return cell;
 	}
 
-	@Override
 	public Document createDocument() {
 		Document document = new Document(PageSize.A4);
 		return document;
 	}
 
-	@Override
 	public Header createHeader() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public PdfPTable createTable(final int columnNumber) {
 		PdfPTable table = new PdfPTable(columnNumber);
 		return table;
 	}
 
-	@Override
 	public void populateTable() {
 		// TODO Auto-generated method stub
 		
