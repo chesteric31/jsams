77a05981d003305f278cdc0b243fa1f57bcf3c08
chesteric31 <chesteric31@users.noreply.github.com>
2010-08-18 07:55:59 +0000

    Creation i18nString class.

---
+++ JSAMS/src/main/java/be/jsams/client/i18n/I18nString.java
@@ -0,0 +1,60 @@
+package be.jsams.client.i18n;
+
+/**
+ * An i18n String / message.
+ * 
+ * 
+ * @author chesteric31
+ * @version $Revision:$ $Date:$ $Author:$
+ */
+public class I18nString {
+
+	private static I18nString emptyString = null;
+
+	public static final Object[] NO_ARGUMENTS = new Object[0];
+
+	private String key;
+
+	private Object[] arguments = NO_ARGUMENTS;
+
+	public I18nString(String key) {
+		this(key, NO_ARGUMENTS);
+	}
+
+	public I18nString(String key, Object[] arguments) {
+		setKey(key);
+		setArguments(arguments);
+	}
+
+	public String getKey() {
+		return key;
+	}
+
+	public void setKey(String key) {
+		this.key = key;
+	}
+
+	public Object[] getArguments() {
+		return arguments;
+	}
+
+	public void setArguments(Object[] arguments) {
+		this.arguments = arguments;
+	}
+
+	public String getTranslation() {
+		return I18nManager.getInstance().translate(this);
+	}
+
+	public static synchronized I18nString getEmptyString() {
+		if (emptyString == null) {
+			emptyString = new I18nString("");
+		}
+
+		return (emptyString);
+	}
+
+	public String toString() {
+		return getTranslation();
+	}
+}
