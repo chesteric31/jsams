d411ad2b5aae1f6a23513ac3a9f639d14128cbeb
chesteric31 <chesteric31@a5fa9106-e037-4f10-9215-0b9fab2b6631>
2010-08-18 14:49:46 +0000

    Adding class javadoc.

--- trunk/JSAMS/src/main/java/be/jsams/server/model/Identity.java
+++ trunk/JSAMS/src/main/java/be/jsams/server/model/Identity.java
@@ -1,5 +1,12 @@
 package be.jsams.server.model;
 
+/**
+ * Identity interface.
+ * 
+ *
+ * @author chesteric31
+ * @version $Revision:$ $Date:$ $Author:$
+ */
 public interface Identity {
 
 	public Long getId();
