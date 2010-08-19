e38695ad892420c99193cec70ff662e331466117
chesteric31 <chesteric31@users.noreply.github.com>
2010-08-19 08:47:14 +0000

    Remove the LOGGER variable, already in the super class.

--- JSAMS/src/main/java/be/jsams/server/dao/impl/SocietyDaoImpl.java
+++ JSAMS/src/main/java/be/jsams/server/dao/impl/SocietyDaoImpl.java
@@ -1,8 +1,5 @@
 package be.jsams.server.dao.impl;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-
 import be.jsams.server.dao.SocietyDao;
 import be.jsams.server.model.Society;
 
@@ -15,8 +12,6 @@ import be.jsams.server.model.Society;
  */
 public class SocietyDaoImpl extends GenericDaoImpl<Society> implements
 		SocietyDao {
-
-	private final Log LOGGER = LogFactory.getLog(this.getClass());
 
 	public SocietyDaoImpl(Class<Society> type) {
 		super(type);
