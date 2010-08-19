66bb7c3d10288b1e02120f94c268310213b88f27
chesteric31 <chesteric31@users.noreply.github.com>
2010-08-19 08:45:52 +0000

    Adding addressDao and implementation.

---
+++ JSAMS/src/main/java/be/jsams/server/dao/AddressDao.java
@@ -0,0 +1,14 @@
+package be.jsams.server.dao;
+
+import be.jsams.server.model.Address;
+
+/**
+ * Address DAO.
+ * 
+ *
+ * @author chesteric31
+ * @version $Revision:$ $Date:$ $Author:$
+ */
+public interface AddressDao extends GenericDao<Address> {
+
+}
