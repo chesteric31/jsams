0b0cc9720ef6b46b854df423f7bd5a2da1df1f7f
chesteric31 <chesteric31@a5fa9106-e037-4f10-9215-0b9fab2b6631>
2010-08-24 13:59:47 +0000

    Adding new service implementation for entities: command and commandDetail.

---
+++ trunk/JSAMS/src/main/java/be/jsams/server/service/impl/CommandServiceImpl.java
@@ -0,0 +1,75 @@
+package be.jsams.server.service.impl;
+
+import java.util.List;
+
+import be.jsams.server.dao.CommandDao;
+import be.jsams.server.dao.CommandDetailDao;
+import be.jsams.server.model.Command;
+import be.jsams.server.model.CommandDetail;
+import be.jsams.server.service.CommandService;
+
+/**
+ * Command service implementation
+ *
+ * @author chesteric31
+ * @version $Rev$ $Date::                  $ $Author$
+ */
+public class CommandServiceImpl implements CommandService {
+
+	private CommandDao commandDao;
+	private CommandDetailDao commandDetailDao;
+	
+	public CommandDao getCommandDao() {
+		return commandDao;
+	}
+
+	public void setCommandDao(CommandDao commandDao) {
+		this.commandDao = commandDao;
+	}
+
+	public CommandDetailDao getCommandDetailDao() {
+		return commandDetailDao;
+	}
+
+	public void setCommandDetailDao(CommandDetailDao commandDetailDao) {
+		this.commandDetailDao = commandDetailDao;
+	}
+
+	public void create(Command command) {
+		commandDao.add(command);
+		List<CommandDetail> details = command.getDetails();
+		for (CommandDetail detail : details) {
+			commandDetailDao.add(detail);
+		}
+	}
+
+	public void delete(Command command) {
+		List<CommandDetail> details = command.getDetails();
+		for (CommandDetail detail : details) {
+			commandDetailDao.remove(detail);
+		}
+		commandDao.remove(command);
+	}
+
+	public void delete(Long id) {
+		Command command = findById(id);
+		List<CommandDetail> details = command.getDetails();
+		for (CommandDetail detail : details) {
+			commandDetailDao.remove(detail);
+		}
+		commandDao.remove(id);
+	}
+
+	public List<Command> findAll() {
+		return commandDao.findAll();
+	}
+
+	public Command findById(Long id) {
+		return commandDao.findById(id);
+	}
+
+	public void update(Command command) {
+		commandDao.update(command);
+	}
+
+}
