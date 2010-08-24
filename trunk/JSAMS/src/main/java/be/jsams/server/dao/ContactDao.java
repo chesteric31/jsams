a3188edb319a2bbd0484c773f42be4a187d962d7
chesteric31 <chesteric31@a5fa9106-e037-4f10-9215-0b9fab2b6631>
2010-08-24 13:54:35 +0000

    Adding new DAO interfaces for entities: command, commandDetail, contact, contactInformation, customer, estimate, estimateDetail, paymentMode, productCategory and product.

---
+++ trunk/JSAMS/src/main/java/be/jsams/server/dao/ContactDao.java
@@ -0,0 +1,13 @@
+package be.jsams.server.dao;
+
+import be.jsams.server.model.Contact;
+
+/**
+ * Contact DAO interface.
+ *
+ * @author chesteric31
+ * @version $Rev$ $Date::                  $ $Author$
+ */
+public interface ContactDao extends GenericDao<Contact> {
+
+}
