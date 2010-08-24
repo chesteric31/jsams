7326e11ee5cd2b252642e839679cf20992d93e36
chesteric31 <chesteric31@a5fa9106-e037-4f10-9215-0b9fab2b6631>
2010-08-24 13:56:48 +0000

    Entity creation CommandDetail.

---
+++ trunk/JSAMS/src/main/java/be/jsams/server/model/CommandDetail.java
@@ -0,0 +1,125 @@
+package be.jsams.server.model;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+/**
+ * Command detail (line) entity object.
+ *
+ * @author chesteric31
+ * @version $Rev$ $Date::                  $ $Author$
+ */
+@Entity
+@Table(name = "COMMAND_DETAIL")
+public class CommandDetail implements Serializable {
+
+	/**
+	 * Serial Version UID
+	 */
+	private static final long serialVersionUID = 3749700047686967069L;
+	private CommandDetailPK id;
+	private int quantity;
+	private BigDecimal price;
+	private BigDecimal vatApplicable;
+	private BigDecimal discountRate;
+	private boolean transferred;
+	
+	private Command command;
+	private Product product;
+
+	public CommandDetail() {
+		super();
+	}
+
+	@EmbeddedId
+	public CommandDetailPK getId() {
+		return id;
+	}
+
+	public void setId(CommandDetailPK id) {
+		this.id = id;
+	}
+
+	@Column(name = "QUANTITY")
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	@Column(name = "PRICE")
+	public BigDecimal getPrice() {
+		return price;
+	}
+
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
+
+	@Column(name = "VAT_APPLICABLE")
+	public BigDecimal getVatApplicable() {
+		return vatApplicable;
+	}
+
+	public void setVatApplicable(BigDecimal vatApplicable) {
+		this.vatApplicable = vatApplicable;
+	}
+
+	@Column(name = "DISCOUNT_RATE")
+	public BigDecimal getDiscountRate() {
+		return discountRate;
+	}
+
+	public void setDiscountRate(BigDecimal discountRate) {
+		this.discountRate = discountRate;
+	}
+
+	@Column(name = "TRANSFERRED")
+	public boolean isTransferred() {
+		return transferred;
+	}
+
+	public void setTransferred(boolean transferred) {
+		this.transferred = transferred;
+	}
+
+	@ManyToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "FK_COMMAND", insertable = false, updatable = false)
+	public Command getCommand() {
+		return command;
+	}
+
+	public void setCommand(Command command) {
+		this.command = command;
+	}
+
+	@ManyToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "FK_PRODUCT")
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+
+	@Override
+	public String toString() {
+		return "CommandeDetail [command=" + command + ", discountRate="
+				+ discountRate + ", id=" + id + ", price=" + price
+				+ ", product=" + product + ", quantity=" + quantity
+				+ ", transferred=" + transferred + ", vatApplicable="
+				+ vatApplicable + "]";
+	}
+
+}
