c3ab9da242fa1ca3d7745bba242f0d8605e98089
chesteric31 <chesteric31@users.noreply.github.com>
2010-08-24 13:56:08 +0000

    Adding new DAO implementation for entities: command and commandDetail.

---
+++ JSAMS/src/main/java/be/jsams/server/dao/impl/CommandDetailDaoImpl.java
@@ -0,0 +1,17 @@
+package be.jsams.server.dao.impl;
+
+import be.jsams.server.dao.CommandDetailDao;
+import be.jsams.server.model.CommandDetail;
+
+/**
+ * Command detail DAO implementation.
+ *
+ * @author chesteric31
+ * @version $Rev$ $Date::                  $ $Author$
+ */
+public class CommandDetailDaoImpl extends GenericDaoImpl<CommandDetail> implements CommandDetailDao {
+
+	public CommandDetailDaoImpl(Class<CommandDetail> type) {
+		super(type);
+	}
+}
