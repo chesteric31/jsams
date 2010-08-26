d8893b08c2e0c956f18f2e9b84c7aa3ebe76c254
chesteric31 <chesteric31@a5fa9106-e037-4f10-9215-0b9fab2b6631>
2010-08-26 14:17:47 +0000

    Civility DAO implementation creation.

---
+++ trunk/JSAMS/src/main/java/be/jsams/server/dao/impl/CivilityDaoImpl.java
@@ -0,0 +1,19 @@
+package be.jsams.server.dao.impl;
+
+import be.jsams.server.dao.CivilityDao;
+import be.jsams.server.model.Civility;
+
+/**
+ * Civility DAO implementation.
+ *
+ * @author chesteric31
+ * @version $Rev$ $Date::                  $ $Author$
+ */
+public class CivilityDaoImpl extends GenericDaoImpl<Civility> implements
+		CivilityDao {
+
+	public CivilityDaoImpl(Class<Civility> type) {
+		super(type);
+	}
+	
+}
