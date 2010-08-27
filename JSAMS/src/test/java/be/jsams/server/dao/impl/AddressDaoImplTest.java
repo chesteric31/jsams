804e82ad6b27195fa30d98315f4f730f233be643
chesteric31 <chesteric31@users.noreply.github.com>
2010-08-27 10:02:37 +0000

    Address DAO junit test creation.

---
+++ JSAMS/src/test/java/be/jsams/server/dao/impl/AddressDaoImplTest.java
@@ -0,0 +1,91 @@
+package be.jsams.server.dao.impl;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import be.jsams.server.dao.AbstractJUnitTestClass;
+import be.jsams.server.dao.AddressDao;
+import be.jsams.server.model.Address;
+
+/**
+ * Test class for {@link AddressDaoImpl}.
+ *
+ * @author chesteric31
+ * @version $Rev$ $Date::                  $ $Author$
+ */
+public class AddressDaoImplTest extends AbstractJUnitTestClass {
+
+	@Autowired
+	private AddressDao dao;
+	
+	private Address address;
+	
+	@Before
+	public void setUp() {
+		address = new Address();
+		address.setCity("Paris");
+		address.setCountry("France");
+		address.setNumber("79");
+		address.setStreet("Rue de Varenne");
+		address.setZipCode(75007);
+	}
+
+	@Test
+	public void testAdd() {
+		dao.add(address);
+		assertNotNull(address.getId());
+	}
+	
+	@Test
+	public void testAddNullCity() {
+		address.setCity(null);
+		dao.add(address);
+		assertNull(address.getId());
+	}
+
+	@Test
+	public void testFindAll() {
+		dao.add(address);
+		Address anOtherAddress = new Address();
+		anOtherAddress.setCity("Brussels");
+		anOtherAddress.setCountry("Belgium");
+		anOtherAddress.setNumber("1");
+		anOtherAddress.setStreet("Rue Neuve");
+		anOtherAddress.setZipCode(1000);
+		dao.add(anOtherAddress);
+		List<Address> addresses = dao.findAll();
+		assertTrue(addresses.size() == 2);
+	}
+
+	@Test
+	public void testFindById() {
+		dao.add(address);
+		Address findAddress = dao.findById(address.getId());
+		assertNotNull(findAddress);
+	}
+
+	@Test
+	public void testRemove() {
+		dao.add(address);
+		dao.remove(address);
+		Address findAddress = dao.findById(address.getId());
+		assertNull(findAddress);
+	}
+
+	@Test
+	public void testUpdate() {
+		dao.add(address);
+		address.setNumber("101");
+		dao.update(address);
+		Address findAddress = dao.findById(address.getId());
+		assertTrue(findAddress.getNumber().equals("101"));
+	}
+
+}
