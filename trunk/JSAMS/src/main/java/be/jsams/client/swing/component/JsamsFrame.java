6b03aa81fa975fc187608254ceffd2b8b5b131fe
chesteric31 <chesteric31@a5fa9106-e037-4f10-9215-0b9fab2b6631>
2010-09-30 10:37:52 +0000

    JsamsFrame generic for application.

---
+++ trunk/JSAMS/src/main/java/be/jsams/client/swing/component/JsamsFrame.java
@@ -0,0 +1,51 @@
+package be.jsams.client.swing.component;
+
+import javax.swing.JFrame;
+import javax.swing.UIManager;
+
+import be.jsams.client.i18n.I18nString;
+
+/**
+ * Generic {@link JFrame} for Jsams application.
+ *
+ * @author chesteric31
+ * @version $Rev$ $Date::                  $ $Author$
+ */
+public class JsamsFrame extends JFrame {
+
+	/**
+	 * Serial Version
+	 */
+	private static final long serialVersionUID = 5732252811794674139L;
+	
+	public JsamsFrame() {
+		super();
+		setNativeLookAndFeel();
+	}
+	
+	public JsamsFrame(final I18nString title) {
+		this();
+		setTitle(title);
+	}
+
+	/**
+	 * Sets the main title of the frame.
+	 * 
+	 * @param title
+	 *            the {@link I18nString} title
+	 */
+	protected void setTitle(final I18nString title) {
+		setTitle(title.getTranslation());
+	}
+
+	/**
+	 * Sets the native look and feel.
+	 */
+	protected void setNativeLookAndFeel() {
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
