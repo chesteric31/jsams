ea0bb0158cf71ef5265548d5002928439b4ae5e7
chesteric31 <chesteric31@users.noreply.github.com>
2010-11-06 11:48:43 +0000

    Replace Contact by Agent, with adding of civility, address and contact informations.

---
+++ JSAMS/src/main/java/be/jsams/server/model/Agent.java
@@ -0,0 +1,104 @@
+package be.jsams.server.model;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+/**
+ * Agent entity object.
+ *
+ * @author chesteric31
+ * @version $Rev$ $Date::                  $ $Author$
+ */
+@Entity
+@Table(name = "AGENT")
+public class Agent extends AbstractIdentity {
+
+	/**
+	 * Serial Version UID
+	 */
+	private static final long serialVersionUID = 8508634686187725716L;
+	private String name;
+	private String function;
+	
+	private List<Customer> customers;
+	private Civility civility;
+	private Address address;
+	private ContactInformation contactInformation;
+
+	public Agent() {
+		super();
+	}
+
+	@Column(name = "NAME")
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Column(name = "FUNCTION")
+	public String getFunction() {
+		return function;
+	}
+
+	public void setFunction(String function) {
+		this.function = function;
+	}
+
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "FK_CIVILITY")
+	public Civility getCivility() {
+		return civility;
+	}
+
+	public void setCivility(Civility civility) {
+		this.civility = civility;
+	}
+
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "FK_ADDRESS")
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	@ManyToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "FK_CONTACT_INFORMATION")
+	public ContactInformation getContactInformation() {
+		return contactInformation;
+	}
+
+	public void setContactInformation(ContactInformation contactInformation) {
+		this.contactInformation = contactInformation;
+	}
+
+	@ManyToMany(mappedBy = "agent")
+	public List<Customer> getCustomers() {
+		return customers;
+	}
+
+	public void setCustomers(List<Customer> customers) {
+		this.customers = customers;
+	}
+
+	@Override
+	public String toString() {
+		return "Agent [address=" + address + ", civility=" + civility
+				+ ", contactInformation=" + contactInformation + ", customers="
+				+ customers + ", function=" + function + ", name=" + name + "]";
+	}
+
+}
