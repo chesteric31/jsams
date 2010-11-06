95a28799031df8cf9690cce2032f844c5ea318c8
chesteric31 <chesteric31@a5fa9106-e037-4f10-9215-0b9fab2b6631>
2010-11-06 11:49:33 +0000

    Replace ContactDao by AgentDao.

---
+++ trunk/JSAMS/src/main/java/be/jsams/server/dao/impl/AgentDaoImpl.java
@@ -0,0 +1,19 @@
+package be.jsams.server.dao.impl;
+
+import be.jsams.server.dao.AgentDao;
+import be.jsams.server.model.Agent;
+
+/**
+ * Agent DAO implementation.
+ *
+ * @author chesteric31
+ * @version $Rev$ $Date::                  $ $Author$
+ */
+public class AgentDaoImpl extends GenericDaoImpl<Agent> implements
+		AgentDao {
+
+	public AgentDaoImpl(Class<Agent> type) {
+		super(type);
+	}
+
+}
