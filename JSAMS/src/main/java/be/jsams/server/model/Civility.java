fcd249ddb6a00450fcbde1bac15524033b1f67a4
chesteric31 <chesteric31@users.noreply.github.com>
2010-11-10 20:33:20 +0000

    Adding labelFr and labelNl Update toString.

--- JSAMS/src/main/java/be/jsams/server/model/Civility.java
+++ JSAMS/src/main/java/be/jsams/server/model/Civility.java
@@ -19,6 +19,8 @@ public class Civility extends AbstractIdentity {
 	 */
 	private static final long serialVersionUID = -7268110575101379157L;
 	private String label;
+	private String labelFr;
+	private String labelNl;
 
 	public Civility() {
 		super();
@@ -33,9 +35,28 @@ public class Civility extends AbstractIdentity {
 		this.label = label;
 	}
 
+	@Column(name = "LABEL_FR")
+	public String getLabelFr() {
+		return labelFr;
+	}
+
+	public void setLabelFr(String labelFr) {
+		this.labelFr = labelFr;
+	}
+
+	@Column(name = "LABEL_NL")
+	public String getLabelNl() {
+		return labelNl;
+	}
+
+	public void setLabelNl(String labelNl) {
+		this.labelNl = labelNl;
+	}
+
 	@Override
 	public String toString() {
-		return "Civility [label=" + label + "]";
+		return "Civility [label=" + label + ", labelFr=" + labelFr
+				+ ", labelNl=" + labelNl + "]";
 	}
-	
+
 }
