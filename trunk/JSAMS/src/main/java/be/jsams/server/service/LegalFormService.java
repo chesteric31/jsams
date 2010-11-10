b2fcb2164476f118782d3e251f0fa955138dfbc0
chesteric31 <chesteric31@a5fa9106-e037-4f10-9215-0b9fab2b6631>
2010-11-10 20:25:11 +0000

    Creation LegalFormService interface and implementation.

---
+++ trunk/JSAMS/src/main/java/be/jsams/server/service/LegalFormService.java
@@ -0,0 +1,28 @@
+package be.jsams.server.service;
+
+import java.util.List;
+
+import be.jsams.server.model.LegalForm;
+
+/**
+ * Legal form interface.
+ * 
+ * 
+ * @author chesteric31
+ * @version $Rev$ $Date:: $ $Author$
+ */
+public interface LegalFormService {
+
+	public void create(LegalForm legalForm);
+
+	public void delete(LegalForm legalForm);
+
+	public void delete(Long id);
+
+	public void update(LegalForm legalForm);
+
+	public LegalForm findById(Long id);
+
+	public List<LegalForm> findAll();
+
+}
