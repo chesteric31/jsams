88921bbd7e8b88329494e36b3af4b6c7f4b26ab5
chesteric31 <chesteric31@users.noreply.github.com>
2010-11-10 20:38:50 +0000

    Adding static fields ENGLISH, DUTCH and FRENCH with methods isEnglish(), isDutch() and isFrench().

--- JSAMS/src/main/java/be/jsams/client/i18n/UserContext.java
+++ JSAMS/src/main/java/be/jsams/client/i18n/UserContext.java
@@ -5,19 +5,36 @@ import java.util.Locale;
 /**
  * Provides an interface for accessing current user related information.
  * 
- *
  * @author chesteric31
  * @version $Rev$ $Date::                  $ $Author$
  */
 public class UserContext {
+
+	public static final String ENGLISH = "en";
+
+	public static final String FRENCH = "fr";
+
+	public static final String DUTCH = "nl";
 
 	public static Locale getLocale() {
 		return Locale.getDefault();
 	}
 
 	public static String getLanguage() {
-		String currentLanguage = UserContext.getLocale().getLanguage();
+		String currentLanguage = getLocale().getLanguage();
 		return currentLanguage;
+	}
+
+	public static boolean isFrench() {
+		return FRENCH.equalsIgnoreCase(getLanguage());
+	}
+
+	public static boolean isDutch() {
+		return DUTCH.equalsIgnoreCase(getLanguage());
+	}
+
+	public static boolean isEnglish() {
+		return !isFrench() && !isDutch();
 	}
 
 }
