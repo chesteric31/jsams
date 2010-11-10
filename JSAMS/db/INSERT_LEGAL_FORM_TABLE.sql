cffa7739cafd8a4c055b7ed663d9eaa30cfd675e
chesteric31 <chesteric31@users.noreply.github.com>
2010-11-10 20:20:20 +0000

    Insert data into civility and legal form tables.

---
+++ JSAMS/db/INSERT_LEGAL_FORM_TABLE.sql
@@ -0,0 +1,18 @@
+INSERT INTO BTS.LEGAL_FORM (LABEL, LABEL_FR, LABEL_NL)
+VALUES ("Public Limited Company", "Entreprise individuelle", "Naamloze Vennootschap (NV)");
+INSERT INTO BTS.LEGAL_FORM (LABEL, LABEL_FR, LABEL_NL)
+VALUES ("Limited Company", "Société Privée à Responsabilité Limitée (Unipersonnelle) (SPRL)", "Personenvennootschap met Beperkte Aansprakelijkheid (PVBA)");
+INSERT INTO BTS.LEGAL_FORM (LABEL, LABEL_FR, LABEL_NL)
+VALUES ("Limited Company Starter", "SPRL Starter (SPRL-S)", "Personenvennootschap met Beperkte Aansprakelijkheid (PVBA Starter)");
+INSERT INTO BTS.LEGAL_FORM (LABEL, LABEL_FR, LABEL_NL)
+VALUES ("Private Limited Company (LTD)", "Société Anonyme (SA)", "Besloten Vennootschap met Beperkte Aansprakelijkheid (BVBA)");
+INSERT INTO BTS.LEGAL_FORM (LABEL, LABEL_FR, LABEL_NL)
+VALUES ("Partnership Limited By Shares", "Société en Commandite par Actions (SCA)", "Commanditaire Vennootschap op Aandelen (CVA)");
+INSERT INTO BTS.LEGAL_FORM (LABEL, LABEL_FR, LABEL_NL)
+VALUES ("Limited Liability Partnership (LLP)", "Société Coopérative à Responsabilité Limitée (SCRL)", "LLP");
+INSERT INTO BTS.LEGAL_FORM (LABEL, LABEL_FR, LABEL_NL)
+VALUES ("Illimited Liability Partnership", "Société Coopérative à Responsabilité Illimitée (SCRI)", "Coöperatieve Vennootschap met Onbeperkte en Hoofdelijke Aansprakelijkheid (CVOA)");
+INSERT INTO BTS.LEGAL_FORM (LABEL, LABEL_FR, LABEL_NL)
+VALUES ("General Partnership", "Société en Nom Collectif (SNC)", "Vennootschap onder Firma (VOF)");
+INSERT INTO BTS.LEGAL_FORM (LABEL, LABEL_FR, LABEL_NL)
+VALUES ("Limited Partnership", "Société en Commandite Simple (SCS)", "Commanditaire Vennootschap (CV)");
