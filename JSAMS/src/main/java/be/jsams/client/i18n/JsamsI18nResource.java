9b613c2314b0b9643d87da87c56f4177dbed9863
chesteric31 <chesteric31@users.noreply.github.com>
2010-11-10 20:39:15 +0000

    Update resource bundles.

--- JSAMS/src/main/java/be/jsams/client/i18n/JsamsI18nResource.java
+++ JSAMS/src/main/java/be/jsams/client/i18n/JsamsI18nResource.java
@@ -1,76 +1,143 @@
 package be.jsams.client.i18n;
 
 /**
+ * Resource file that contains all {@link I18nString} properties key.
  * 
- *
  * @author chesteric31
  * @version $Rev$ $Date::                  $ $Author$
  */
 public class JsamsI18nResource {
-	
-	public static final I18nString APPLICATION_TITLE = new I18nString("application.title");
-	
-	public static final I18nString EXIT_APPLICATION_CONFIRMATION = new I18nString("exit.confirmation");
-	
+
+	public static final I18nString TITLE_CHOOSE_SOCIETY = new I18nString(
+			"title.choose.society");
+
+	public static final I18nString LABEL_CHOOSE_SOCIETY = new I18nString(
+			"label.choose.society");
+
+	public static final I18nString LABEL_CHOOSE_SOCIETY_AVAILABLES = new I18nString(
+			"label.choose.society_availables");
+
+	public static final I18nString BUTTON_CHOOSE_SOCIETIES_NEW = new I18nString(
+			"button.choose.society_new");
+
+	public static final I18nString EXIT_APPLICATION_CONFIRMATION = new I18nString(
+			"exit.confirmation");
+
 	public static final I18nString MENU_FILE = new I18nString("menu.file");
-	
+
 	public static final I18nString MENU_EDIT = new I18nString("menu.edit");
-	
+
 	public static final I18nString MENU_SALES = new I18nString("menu.sales");
-	
+
 	public static final I18nString MENU_HELP = new I18nString("menu.help");
-	
-	public static final I18nString MENU_ITEM_SOCIETY_PARAMETERS = new I18nString("menu.item.society_parameters");
-	
-	public static final I18nString MENU_ITEM_PRINTER_PARAMETERS = new I18nString("menu.item.printer_parameters");
-	
-	public static final I18nString MENU_ITEM_EXIT_APPLICATION = new I18nString("menu.item.exit");
-	
-	public static final I18nString MENU_ITEM_CUT = new I18nString("menu.item.cut");
-	
-	public static final I18nString MENU_ITEM_COPY = new I18nString("menu.item.copy");
-	
-	public static final I18nString MENU_ITEM_PASTE = new I18nString("menu.item.paste");
-	
-	public static final I18nString MENU_ITEM_HELP = new I18nString("menu.item.help");
-	
-	public static final I18nString MENU_ITEM_ABOUT = new I18nString("menu.item.about");
-	
-	public static final I18nString MENU_ITEM_ESTIMATE = new I18nString("menu.item.estimate");
-	
-	public static final I18nString MENU_ITEM_COMMAND = new I18nString("menu.item.command");
-	
-	public static final I18nString MENU_ITEM_DELIVERY_ORDER = new I18nString("menu.item.delivery_order");
-	
-	public static final I18nString MENU_ITEM_BILL = new I18nString("menu.item.bill");
-	
-	public static final I18nString MENU_ITEM_CREDIT_NOTE = new I18nString("menu.item.credit_note");
-	
-	public static final I18nString TITLE_EDIT_SOCIETY_PARAMETERS = new I18nString("title.edit.society_parameters");
-	
+
+	public static final I18nString MENU_ITEM_SOCIETY_PARAMETERS = new I18nString(
+			"menu.item.society_parameters");
+
+	public static final I18nString MENU_ITEM_PRINTER_PARAMETERS = new I18nString(
+			"menu.item.printer_parameters");
+
+	public static final I18nString MENU_ITEM_EXIT_APPLICATION = new I18nString(
+			"menu.item.exit");
+
+	public static final I18nString MENU_ITEM_CUT = new I18nString(
+			"menu.item.cut");
+
+	public static final I18nString MENU_ITEM_COPY = new I18nString(
+			"menu.item.copy");
+
+	public static final I18nString MENU_ITEM_PASTE = new I18nString(
+			"menu.item.paste");
+
+	public static final I18nString MENU_ITEM_HELP = new I18nString(
+			"menu.item.help");
+
+	public static final I18nString MENU_ITEM_ABOUT = new I18nString(
+			"menu.item.about");
+
+	public static final I18nString MENU_ITEM_ESTIMATE = new I18nString(
+			"menu.item.estimate");
+
+	public static final I18nString MENU_ITEM_COMMAND = new I18nString(
+			"menu.item.command");
+
+	public static final I18nString MENU_ITEM_DELIVERY_ORDER = new I18nString(
+			"menu.item.delivery_order");
+
+	public static final I18nString MENU_ITEM_BILL = new I18nString(
+			"menu.item.bill");
+
+	public static final I18nString MENU_ITEM_CREDIT_NOTE = new I18nString(
+			"menu.item.credit_note");
+
+	public static final I18nString TITLE_EDIT_SOCIETY_PARAMETERS = new I18nString(
+			"title.edit.society_parameters");
+
 	public static final I18nString LABEL_NAME = new I18nString("label.name");
-	
+
 	public static final I18nString LABEL_STREET = new I18nString("label.street");
-	
+
 	public static final I18nString LABEL_NUMBER = new I18nString("label.number");
-	
+
 	public static final I18nString LABEL_BOX = new I18nString("label.box");
-	
-	public static final I18nString LABEL_ZIP_CODE = new I18nString("label.zip_code");
-	
+
+	public static final I18nString LABEL_ZIP_CODE = new I18nString(
+			"label.zip_code");
+
 	public static final I18nString LABEL_CITY = new I18nString("label.city");
-	
-	public static final I18nString LABEL_COUNTRY = new I18nString("label.country");
-	
+
+	public static final I18nString LABEL_COUNTRY = new I18nString(
+			"label.country");
+
 	public static final I18nString BUTTON_OK = new I18nString("button.ok");
-	
-	public static final I18nString BUTTON_CANCEL = new I18nString("button.cancel");
-	
-	public static final I18nString PANEL_ADDRESS_TITLE = new I18nString("panel.address.title");
-	
-	public static final I18nString PANEL_DETAILS_TITLE = new I18nString("panel.details.title");
-	
-	public static final I18nString PANEL_CONTACT_INFORMATIONS_TITLE
-		= new I18nString("panel.contact_informations.title");
+
+	public static final I18nString BUTTON_CANCEL = new I18nString(
+			"button.cancel");
+
+	public static final I18nString LABEL_ADDRESS = new I18nString(
+			"label.address");
+
+	public static final I18nString LABEL_DETAILS = new I18nString(
+			"label.details");
+
+	public static final I18nString PANEL_CONTACT_INFORMATIONS_TITLE = new I18nString(
+			"panel.contact_informations.title");
+
+	public static final I18nString BUTTON_RESET = new I18nString(
+			"button.reset");
+
+	public static final I18nString TITLE_EDIT_SOCIETY = new I18nString(
+			"title.edit.society");
+
+	public static final I18nString LABEL_CONTACT_INFORMATIONS = new I18nString(
+			"label.contact_informations");
+
+	public static final I18nString LABEL_PHONE = new I18nString("label.phone");
+
+	public static final I18nString LABEL_FAX = new I18nString("label.fax");
+
+	public static final I18nString LABEL_MOBILE = new I18nString("label.mobile");
+
+	public static final I18nString LABEL_EMAIL = new I18nString("label.email");
+
+	public static final I18nString LABEL_WEBSITE = new I18nString(
+			"label.website");
+
+	public static final I18nString LABEL_MISC = new I18nString("label.misc");
+
+	public static final I18nString LABEL_LEGAL_FORM = new I18nString(
+			"label.legal_form");
+
+	public static final I18nString LABEL_CAPITAL = new I18nString(
+			"label.capital");
+
+	public static final I18nString LABEL_ACTIVITY = new I18nString(
+			"label.activity");
+
+	public static final I18nString LABEL_RESPONSIBLE = new I18nString(
+			"label.responsible");
+
+	public static final I18nString LABEL_VAT_NUMBER = new I18nString(
+			"label.vat_number");
 
 }
