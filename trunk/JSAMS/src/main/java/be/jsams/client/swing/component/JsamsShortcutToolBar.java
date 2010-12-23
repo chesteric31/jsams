9027ad9173dd47810abb7f3ae0b56a998b134a0e
chesteric31 <chesteric31@a5fa9106-e037-4f10-9215-0b9fab2b6631>
2010-12-23 15:06:22 +0000

    Adding new jsams swing components.

---
+++ trunk/JSAMS/src/main/java/be/jsams/client/swing/component/JsamsShortcutToolBar.java
@@ -0,0 +1,50 @@
+package be.jsams.client.swing.component;
+
+import java.awt.FlowLayout;
+
+import javax.swing.JPanel;
+
+import be.jsams.client.i18n.JsamsI18nResource;
+import be.jsams.client.swing.listener.NewCustomerListener;
+
+/**
+ * 
+ *
+ * @author chesteric31
+ * @version $Rev$ $Date::                  $ $Author$
+ */
+public class JsamsShortcutToolBar extends JPanel {
+
+	/**
+	 * Serial Version
+	 */
+	private static final long serialVersionUID = 8357027337848797984L;
+	
+	private JsamsButton newCustomerButton;
+	private JsamsButton newEstimateButton;
+	private JsamsButton newBillButton;
+	private JsamsButton newProductButton;
+	private JsamsButton statisticsButton;
+	
+	public JsamsShortcutToolBar() {
+		super();
+		initComponents();
+	}
+	
+	private void initComponents() {
+		JPanel panel = new JPanel(new FlowLayout());
+		newCustomerButton = new JsamsButton(JsamsI18nResource.BUTTON_NEW_CUSTOMER);
+		newCustomerButton.addActionListener(new NewCustomerListener());
+		newEstimateButton = new JsamsButton(JsamsI18nResource.BUTTON_NEW_ESTIMATE);
+		newBillButton = new JsamsButton(JsamsI18nResource.BUTTON_NEW_BILL);
+		newProductButton = new JsamsButton(JsamsI18nResource.BUTTON_NEW_PRODUCT);
+		statisticsButton = new JsamsButton(JsamsI18nResource.BUTTON_STATISTICS);
+		
+		panel.add(newCustomerButton);
+		panel.add(newEstimateButton);
+		panel.add(newBillButton);
+		panel.add(newProductButton);
+		panel.add(statisticsButton);
+		this.add(panel);
+	}
+}
---
+++ trunk/JSAMS/src/main/java/be/jsams/client/swing/component/JsamsTextField.java
@@ -0,0 +1,35 @@
+package be.jsams.client.swing.component;
+
+import javax.swing.JTextField;
+
+import be.jsams.client.i18n.I18nString;
+
+/**
+ * 
+ *
+ * @author chesteric31
+ * @version $Rev$ $Date::                  $ $Author$
+ */
+public class JsamsTextField extends JTextField {
+
+	/**
+	 * Serial Version UID
+	 */
+	private static final long serialVersionUID = 410786496726208439L;
+
+	private String label;
+	
+	public JsamsTextField(final int columns, final I18nString label) {
+		super(columns);
+		this.label = label.getTranslation();
+	}
+
+	public String getLabel() {
+		return label;
+	}
+
+	public void setLabel(I18nString label) {
+		this.label = label.getTranslation();
+	}
+	
+}
