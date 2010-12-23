90642a5d534833345e6426bd3a5f6de3f2c73aea
chesteric31 <chesteric31@a5fa9106-e037-4f10-9215-0b9fab2b6631>
2010-12-23 19:15:27 +0000

    Adding constructor.

--- trunk/JSAMS/src/main/java/be/jsams/client/swing/component/JsamsButtonsFrame.java
+++ trunk/JSAMS/src/main/java/be/jsams/client/swing/component/JsamsButtonsFrame.java
@@ -18,11 +18,15 @@ public abstract class JsamsButtonsFrame extends JsamsFrame {
 	private static final long serialVersionUID = -7641496562484006756L;
 	
 	private JsamsButtonsPanel buttonsPanel;
-	
-	public JsamsButtonsFrame(final I18nString title) {
-		super(title);
+
+	public JsamsButtonsFrame(final I18nString title, final String iconFileName) {
+		super(title, iconFileName);
 		buttonsPanel = new JsamsButtonsPanel(this, true, true, true);
 		add(buttonsPanel, BorderLayout.SOUTH);
+	}
+	
+	public JsamsButtonsFrame(final I18nString title) {
+		this(title, null);
 	}
 	
 	protected abstract void performOk();
