ad4b976bef7f50f7c6bb1e82d757d60866e75aa9
chesteric31 <chesteric31@a5fa9106-e037-4f10-9215-0b9fab2b6631>
2010-12-23 19:13:49 +0000

    Replace newcustomerlistenr by newcustomeraction.

---
+++ trunk/JSAMS/src/main/java/be/jsams/client/swing/action/NewCustomerAction.java
@@ -0,0 +1,31 @@
+package be.jsams.client.swing.action;
+
+import java.awt.event.ActionEvent;
+
+import javax.swing.AbstractAction;
+
+import be.jsams.client.desktop.EditCustomerFrame;
+import be.jsams.client.i18n.JsamsI18nResource;
+
+/**
+ * {@link AbstractAction} to launch {@link EditCustomerFrame}.
+ * 
+ * @author chesteric31
+ * @version $Rev$ $Date::                  $ $Author$
+ */
+public class NewCustomerAction extends AbstractAction {
+
+	/**
+	 * Serial Version UID
+	 */
+	private static final long serialVersionUID = -872702115060030751L;
+
+	public void actionPerformed(ActionEvent e) {
+		EditCustomerFrame editCustomerFrame = new EditCustomerFrame(
+				JsamsI18nResource.TITLE_EDIT_CUSTOMER, null);
+		editCustomerFrame.setLocationRelativeTo(null);
+		editCustomerFrame.setResizable(false);
+		editCustomerFrame.setVisible(true);
+	}
+
+}
