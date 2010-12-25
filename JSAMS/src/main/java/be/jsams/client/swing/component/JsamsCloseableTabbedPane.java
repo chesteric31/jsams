95eda39c6f8bd025821c0464e9841c9735d8bc2c
chesteric31 <chesteric31@users.noreply.github.com>
2010-12-25 19:01:44 +0000

    Add title and default icon.

--- JSAMS/src/main/java/be/jsams/client/swing/component/JsamsCloseableTabbedPane.java
+++ JSAMS/src/main/java/be/jsams/client/swing/component/JsamsCloseableTabbedPane.java
@@ -5,6 +5,7 @@ import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
 
@@ -12,10 +13,11 @@ import be.jsams.client.i18n.I18nString;
 import be.jsams.client.swing.utils.IconUtil;
 
 /**
- * 
+ * An extension of {@link JTabbedPane} with a closing button and a icon that
+ * represents the business.
  * 
  * @author chesteric31
- * @version $Rev$ $Date::   $Author$
+ * @version $$Rev$$ $$Date::                  $$ $$Author$$
  */
 public class JsamsCloseableTabbedPane extends JTabbedPane {
 
@@ -28,28 +30,38 @@ public class JsamsCloseableTabbedPane extends JTabbedPane {
 		super();
 	}
 
-	public void addTab(I18nString title, Component component) {
-		final JPanel content = new JPanel();
+	public void addTab(I18nString title, final String iconFileName,
+			final Component component) {
 		JPanel tab = new JPanel();
 		tab.setOpaque(false);
 
-		JsamsLabel tabLabel = new JsamsLabel(title);
-		JsamsButton tabCloseButton = new JsamsButton(
-				IconUtil.MENU_ICON_PREFIX + "emblems/emblem-unreadable.png");
+		JsamsLabel tabTitle = null;
+		ImageIcon tabIcon = null;
+		if (iconFileName != null) {
+			tabIcon = new ImageIcon(IconUtil
+					.buildIcon(IconUtil.MENU_ICON_PREFIX + iconFileName));
+			tabTitle = new JsamsLabel(title, tabIcon);
+		} else {
+			tabTitle = new JsamsLabel(title);
+		}
+
+		JsamsButton tabCloseButton = new JsamsButton(IconUtil.MENU_ICON_PREFIX
+				+ "emblems/emblem-unreadable.png");
 		tabCloseButton.setPreferredSize(new Dimension(tabCloseButton.getIcon()
 				.getIconWidth(), tabCloseButton.getIcon().getIconHeight()));
 		tabCloseButton.addActionListener(new ActionListener() {
 
 			public void actionPerformed(ActionEvent e) {
-				int closeTabNumber = indexOfComponent(content);
+				int closeTabNumber = indexOfComponent(component);
 				removeTabAt(closeTabNumber);
 			}
 		});
 
-		tab.add(tabLabel);
+		tab.add(tabTitle);
 		tab.add(tabCloseButton);
 
-		super.addTab(null, content);
+		super.addTab(null, component);
 		setTabComponentAt(getTabCount() - 1, tab);
+		setSelectedComponent(component);
 	}
 }
