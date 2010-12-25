7500afb0e434a1329d2654ab9fb397e8c9401508
chesteric31 <chesteric31@users.noreply.github.com>
2010-12-25 21:02:41 +0000

    Remove genericcomboboxrenderer by 2 news renderer for columns: name and label.

---
+++ JSAMS/src/main/java/be/jsams/client/renderer/NamedComboBoxRenderer.java
@@ -0,0 +1,36 @@
+package be.jsams.client.renderer;
+
+import java.awt.Component;
+
+import javax.swing.DefaultListCellRenderer;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.ListCellRenderer;
+
+import be.jsams.server.model.AbstractNamedIdentity;
+
+/**
+ * Combo box renderer for object that have name column from DB.
+ * 
+ * @author chesteric31
+ *  @version $$Rev$$ $$Date::                  $$ $$Author$$
+ */
+public class NamedComboBoxRenderer implements ListCellRenderer {
+
+	protected DefaultListCellRenderer defaultRenderer = new DefaultListCellRenderer();
+
+	public Component getListCellRendererComponent(JList list, Object value,
+			int index, boolean isSelected, boolean cellHasFocus) {
+		String theText = " ";
+
+		JLabel renderer = (JLabel) defaultRenderer
+				.getListCellRendererComponent(list, value, index, isSelected,
+						cellHasFocus);
+		if (value != null) {
+			theText = ((AbstractNamedIdentity) value).getName();
+		}
+		renderer.setText(theText);
+		return renderer;
+	}
+
+}
---
+++ JSAMS/src/main/java/be/jsams/client/renderer/TranslatableComboBoxRenderer.java
@@ -0,0 +1,43 @@
+package be.jsams.client.renderer;
+
+import java.awt.Component;
+
+import javax.swing.DefaultListCellRenderer;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.ListCellRenderer;
+
+import be.jsams.client.i18n.UserContext;
+import be.jsams.server.model.AbstractTranslatableIdentity;
+
+/**
+ * Combo box renderer for object that have translatable label from DB.
+ * 
+ * @author chesteric31
+ * @version $Rev$ $Date::                  $ $Author$
+ */
+public class TranslatableComboBoxRenderer implements ListCellRenderer {
+	
+	protected DefaultListCellRenderer defaultRenderer = new DefaultListCellRenderer();
+
+	public Component getListCellRendererComponent(JList list, Object value,
+			int index, boolean isSelected, boolean cellHasFocus) {
+		String theText = " ";
+
+		JLabel renderer = (JLabel) defaultRenderer
+				.getListCellRendererComponent(list, value, index, isSelected,
+						cellHasFocus);
+		if (value != null) {
+			if (UserContext.isFrench()) {
+				theText = ((AbstractTranslatableIdentity) value).getLabelFr();
+			} else if (UserContext.isDutch()) {
+				theText = ((AbstractTranslatableIdentity) value).getLabelNl();
+			} else {
+				theText = ((AbstractTranslatableIdentity) value).getLabel();
+			}
+		}
+		renderer.setText(theText);
+		return renderer;
+	}
+	
+}
