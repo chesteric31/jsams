6445110feb69c1bd23ee633d7ef8d92225a077c7
chesteric31 <chesteric31@a5fa9106-e037-4f10-9215-0b9fab2b6631>
2010-12-29 21:11:59 +0000

    Update table model for product.

--- trunk/JSAMS/src/main/java/be/jsams/client/model/table/ProductTableModel.java
+++ trunk/JSAMS/src/main/java/be/jsams/client/model/table/ProductTableModel.java
@@ -5,13 +5,15 @@ import java.util.List;
 
 import javax.swing.table.AbstractTableModel;
 
+import be.jsams.client.i18n.I18nString;
+import be.jsams.client.i18n.JsamsI18nResource;
 import be.jsams.server.model.Product;
 
 /**
- * 
+ * {@link AbstractTableModel} for {@link Product} object.
  * 
  * @author chesteric31
- * @version $Rev$ $Date:: $ $Author$
+ * @version $Rev$ $Date::   $Author$
  */
 public class ProductTableModel extends AbstractTableModel {
 
@@ -20,8 +22,13 @@ public class ProductTableModel extends AbstractTableModel {
 	 */
 	private static final long serialVersionUID = 5631609209979319706L;
 
-	private List<String> columnNames = Arrays.asList("ID", "NAME", "PRICE",
-			"QTE_STOCK", "REORDER_LEVEL", "VAT_APPLICABLE", "CATEGORY");
+	private List<I18nString> columnNames = Arrays.asList(
+			JsamsI18nResource.COLUMN_ID, JsamsI18nResource.COLUMN_NAME,
+			JsamsI18nResource.COLUMN_PRICE,
+			JsamsI18nResource.COLUMN_STOCK_QUANTITY,
+			JsamsI18nResource.COLUMN_REORDER_LEVEL,
+			JsamsI18nResource.COLUMN_VAT_APPLICABE,
+			JsamsI18nResource.COLUMN_PRODUCT_CATEGORY);
 
 	private List<Product> data;
 
@@ -48,22 +55,22 @@ public class ProductTableModel extends AbstractTableModel {
 		case 1:
 			return data.get(rowIndex).getName();
 		case 2:
-			return data.get(rowIndex).getPrice();
+			return data.get(rowIndex).getPrice().toPlainString();
 		case 3:
 			return data.get(rowIndex).getQuantityStock();
 		case 4:
 			return data.get(rowIndex).getReorderLevel();
 		case 5:
-			return data.get(rowIndex).getVatApplicable();
+			return data.get(rowIndex).getVatApplicable().toPlainString();
 		case 6:
-			return data.get(rowIndex).getCategory();
+			return data.get(rowIndex).getCategory().getName();
 		default:
 			return "";
 		}
 	}
 
 	public String getColumnName(int columnIndex) {
-		return columnNames.get(columnIndex);
+		return columnNames.get(columnIndex).getTranslation();
 	}
 
 }
