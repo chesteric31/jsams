b68d6e8ac3a3fba1de93d56b5905cf8f81ee94a6
chesteric31 <chesteric31@users.noreply.github.com>
2011-01-02 19:16:14 +0000

    New table mouse listener.

---
+++ JSAMS/src/main/java/be/jsams/client/swing/listener/TableMouseListener.java
@@ -0,0 +1,69 @@
+package be.jsams.client.swing.listener;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import be.jsams.client.context.JsamsApplicationContext;
+import be.jsams.client.i18n.JsamsI18nResource;
+import be.jsams.client.model.dialog.EditProductDialog;
+import be.jsams.client.swing.component.JsamsTable;
+import be.jsams.server.model.Product;
+
+/**
+ * Customized {@link MouseListener} for {@link JsamsTable} double click.
+ *
+ * @author chesteric31
+ * @version $Rev$ $Date::                  $ $Author$
+ */
+public class TableMouseListener implements MouseListener {
+
+    /**
+     * {@inheritDoc}
+     */
+    public void mouseClicked(MouseEvent e) {
+        JsamsTable table = (JsamsTable) e.getSource();
+        if (e.getClickCount() == 2) {
+            int selectedRow = table.getSelectedRow();
+            if (selectedRow > -1) {
+                Long id = (Long) table.getValueAt(selectedRow, 0);
+                Product selectedProduct = JsamsApplicationContext
+                        .getProductService().findById(id);
+                new EditProductDialog(JsamsI18nResource.TITLE_EDIT_PRODUCT,
+                        selectedProduct);
+            }
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void mouseEntered(MouseEvent e) {
+        // TODO Auto-generated method stub
+
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void mouseExited(MouseEvent e) {
+        // TODO Auto-generated method stub
+
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void mousePressed(MouseEvent e) {
+        // TODO Auto-generated method stub
+
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void mouseReleased(MouseEvent e) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
