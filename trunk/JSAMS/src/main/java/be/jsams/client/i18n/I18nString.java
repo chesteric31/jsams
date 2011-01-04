db3b827c6bce03e562858770e4c7494d2dea4436
chesteric31 <chesteric31@a5fa9106-e037-4f10-9215-0b9fab2b6631>
2011-01-04 09:57:17 +0000

    Formatting & javadoc.

--- trunk/JSAMS/src/main/java/be/jsams/client/i18n/I18nString.java
+++ trunk/JSAMS/src/main/java/be/jsams/client/i18n/I18nString.java
@@ -3,7 +3,7 @@ package be.jsams.client.i18n;
 /**
  * An i18n String / message.
  * 
- *
+ * 
  * @author chesteric31
  * @version $Rev$ $Date::                  $ $Author$
  */
@@ -17,35 +17,75 @@ public class I18nString {
 
 	private Object[] arguments = NO_ARGUMENTS;
 
+	/**
+	 * Constructor
+	 * 
+	 * @param key
+	 *            the key resource bundle
+	 */
 	public I18nString(String key) {
 		this(key, NO_ARGUMENTS);
 	}
 
+	/**
+	 * Constructor
+	 * 
+	 * @param key
+	 *            the key resource bundle
+	 * @param arguments
+	 *            the table arguments
+	 */
 	public I18nString(String key, Object[] arguments) {
 		setKey(key);
 		setArguments(arguments);
 	}
 
+	/**
+	 * 
+	 * @return the key
+	 */
 	public String getKey() {
 		return key;
 	}
 
+	/**
+	 * 
+	 * @param key
+	 *            the key to set
+	 */
 	public void setKey(String key) {
 		this.key = key;
 	}
 
+	/**
+	 * 
+	 * @return the arguments table
+	 */
 	public Object[] getArguments() {
 		return arguments;
 	}
 
+	/**
+	 * 
+	 * @param arguments
+	 *            the table arguments to set
+	 */
 	public void setArguments(Object[] arguments) {
 		this.arguments = arguments;
 	}
 
+	/**
+	 * 
+	 * @return the {@link String} translation
+	 */
 	public String getTranslation() {
 		return I18nManager.getInstance().translate(this);
 	}
 
+	/**
+	 * 
+	 * @return the empty {@link I18nString}
+	 */
 	public static synchronized I18nString getEmptyString() {
 		if (emptyString == null) {
 			emptyString = new I18nString("");
--- trunk/JSAMS/src/main/java/be/jsams/client/i18n/UserContext.java
+++ trunk/JSAMS/src/main/java/be/jsams/client/i18n/UserContext.java
@@ -16,23 +16,43 @@ public class UserContext {
 
 	public static final String DUTCH = "nl";
 
+	/**
+	 * 
+	 * @return the default {@link Locale}
+	 */
 	public static Locale getLocale() {
 		return Locale.getDefault();
 	}
 
+	/**
+	 * 
+	 * @return the default Language
+	 */
 	public static String getLanguage() {
 		String currentLanguage = getLocale().getLanguage();
 		return currentLanguage;
 	}
 
+	/**
+	 * 
+	 * @return true if French speaking
+	 */
 	public static boolean isFrench() {
 		return FRENCH.equalsIgnoreCase(getLanguage());
 	}
 
+	/**
+	 * 
+	 * @return true if Dutch speaking
+	 */
 	public static boolean isDutch() {
 		return DUTCH.equalsIgnoreCase(getLanguage());
 	}
 
+	/**
+	 * 
+	 * @return true if English speaking (not French and not Dutch)
+	 */
 	public static boolean isEnglish() {
 		return !isFrench() && !isDutch();
 	}
