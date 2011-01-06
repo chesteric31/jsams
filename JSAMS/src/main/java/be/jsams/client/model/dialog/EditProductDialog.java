74b64e5f637603ede340039d7ed97aeb299dad04
chesteric31 <chesteric31@users.noreply.github.com>
2011-01-06 10:15:22 +0000

    Change editproductdialog to editdialog generic kid.

--- JSAMS/src/main/java/be/jsams/client/model/dialog/EditProductDialog.java
+++ JSAMS/src/main/java/be/jsams/client/model/dialog/EditProductDialog.java
@@ -4,43 +4,40 @@ import java.awt.BorderLayout;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.swing.BoxLayout;
+import javax.swing.BorderFactory;
 import javax.swing.JComboBox;
 import javax.swing.JPanel;
 
 import be.jsams.client.context.JsamsApplicationContext;
 import be.jsams.client.i18n.I18nString;
 import be.jsams.client.i18n.JsamsI18nLabelResource;
-import be.jsams.client.renderer.NamedComboBoxRenderer;
-import be.jsams.client.swing.component.JsamsButtonsInterface;
-import be.jsams.client.swing.component.JsamsButtonsPanel;
+import be.jsams.client.renderer.TranslatableComboBoxRenderer;
 import be.jsams.client.swing.component.JsamsDialog;
 import be.jsams.client.swing.component.JsamsFrame;
-import be.jsams.client.swing.component.JsamsStatusBar;
 import be.jsams.client.swing.component.JsamsTextField;
+import be.jsams.client.validator.ProductValidator;
 import be.jsams.server.model.Product;
 import be.jsams.server.model.ProductCategory;
+import be.jsams.server.service.ProductService;
 
 import com.jgoodies.forms.builder.DefaultFormBuilder;
 import com.jgoodies.forms.layout.FormLayout;
-import com.jgoodies.validation.ValidationResultModel;
-import com.jgoodies.validation.util.DefaultValidationResultModel;
 import com.jgoodies.validation.view.ValidationComponentUtils;
 
 /**
  * Edit Product {@link JsamsDialog}, to create or update a Product object.
  * 
  * @author chesteric31
- * @version $Rev$ $Date::   $Author$
+ * @version $$Rev$$ $$Date::                  $$ $$Author$$
  */
-public class EditProductDialog extends JsamsDialog implements
-		JsamsButtonsInterface {
+public class EditProductDialog extends
+		EditDialog<Product, ProductValidator, ProductService> {
 
 	/**
 	 * Serial Version UID
 	 */
 	private static final long serialVersionUID = -5931469580616365674L;
-	
+
 	private static final int MAX_CHARACTERS = 50;
 
 	private static final int MAX_NUMBERS = 10;
@@ -53,18 +50,8 @@ public class EditProductDialog extends JsamsDialog implements
 			MAX_NUMBERS);
 	private JsamsTextField textFieldVatApplicable = new JsamsTextField(
 			MAX_NUMBERS);
-	
-	private Product model;
 
-	private JsamsButtonsPanel buttonsPanel;
-	
 	private JComboBox comboBoxProductCategory;
-
-	private ValidationResultModel validationResultModel = new DefaultValidationResultModel();
-
-	private JsamsStatusBar statusBar;
-
-	private JPanel southPanel;
 
 	/**
 	 * Constructor
@@ -76,36 +63,13 @@ public class EditProductDialog extends JsamsDialog implements
 	 */
 	public EditProductDialog(final I18nString title, Product model) {
 		super(null, title);
-		this.model = model;
-		buttonsPanel = new JsamsButtonsPanel(this, true, true, true);
-		add(buttonsPanel, BorderLayout.SOUTH);
+		super.setModel(model);
+		super.setValidator(new ProductValidator());
+		super.setService(JsamsApplicationContext.getProductService());
 		initComponents();
-	}
-
-	public ValidationResultModel getValidationResultModel() {
-		return validationResultModel;
-	}
-
-	public void setValidationResultModel(
-			ValidationResultModel validationResultModel) {
-		this.validationResultModel = validationResultModel;
-	}
-
-	/**
-	 * 
-	 * @return the {@link Product}
-	 */
-	public Product getModel() {
-		return model;
-	}
-
-	/**
-	 * 
-	 * @param model
-	 *            the {@link Product} to set
-	 */
-	public void setModel(Product model) {
-		this.model = model;
+		setLocationRelativeTo(null);
+		setResizable(false);
+		setVisible(true);
 	}
 
 	/**
@@ -114,15 +78,13 @@ public class EditProductDialog extends JsamsDialog implements
 	private void initComponents() {
 		fillData();
 		FormLayout layout = new FormLayout("right:p, 3dlu, p:grow, 3dlu, "
-				+ "right:p, 3dlu, p:grow, 3dlu, "
-				+ "right:p, 3dlu, p:grow, 3dlu, " + "right:p, 3dlu, p:grow",
-				"p");
+				+ "right:p, 3dlu, p:grow", "p");
 		DefaultFormBuilder builder = new DefaultFormBuilder(layout,
 				JsamsFrame.RESOURCE_BUNDLE);
 		builder.appendI15d(JsamsI18nLabelResource.LABEL_PRODUCT_LABEL.getKey(),
 				textFieldLabel, 5);
-		builder.appendI15d(JsamsI18nLabelResource.LABEL_PRODUCT_CATEGORY.getKey(),
-				comboBoxProductCategory, 5);
+		builder.appendI15d(JsamsI18nLabelResource.LABEL_PRODUCT_CATEGORY
+				.getKey(), comboBoxProductCategory, 5);
 		builder.nextLine();
 		builder.appendI15d(JsamsI18nLabelResource.LABEL_PRODUCT_PRICE.getKey(),
 				textFieldPrice, 1);
@@ -139,39 +101,13 @@ public class EditProductDialog extends JsamsDialog implements
 		mainPanel.setLayout(new BorderLayout());
 		mainPanel.add(builder.getPanel(), BorderLayout.CENTER);
 		ValidationComponentUtils
-				.updateComponentTreeMandatoryBackground(mainPanel);
+				.updateComponentTreeMandatoryBorder(this);
 		add(mainPanel);
-		
-		buildSouthPanel();
-		add(southPanel, BorderLayout.SOUTH);
-		
+
 		pack();
-		setLocationRelativeTo(null);
-		setVisible(true);
-		setResizable(false);
-
-	}
-	
-	private void buildSouthPanel() {
-		statusBar = new JsamsStatusBar();
-		southPanel = new JPanel();
-		southPanel.setLayout(new BoxLayout(southPanel, BoxLayout.PAGE_AXIS));
-		buttonsPanel = new JsamsButtonsPanel(this, true, true, true);
-		southPanel.add(buttonsPanel);
-		southPanel.add(statusBar);
-	}
-
-	public void performCancel() {
-		// TODO Auto-generated method stub
-
 	}
 
 	public void performOk() {
-		// TODO Auto-generated method stub
-
-	}
-
-	public void performReset() {
 		// TODO Auto-generated method stub
 
 	}
@@ -196,13 +132,18 @@ public class EditProductDialog extends JsamsDialog implements
 			textFieldVatApplicable.setText(product.getVatApplicable()
 					.toPlainString());
 		}
-		comboBoxProductCategory.setRenderer(new NamedComboBoxRenderer());
+		comboBoxProductCategory.setRenderer(new TranslatableComboBoxRenderer());
 	}
 
 	private void setMandatoryFields() {
 		ValidationComponentUtils.setMandatory(textFieldLabel, true);
 		ValidationComponentUtils.setMandatory(textFieldPrice, true);
 		ValidationComponentUtils.setMandatory(textFieldVatApplicable, true);
+		ValidationComponentUtils.setMandatory(textFieldStockQuantity, true);
+		ValidationComponentUtils.setMandatory(comboBoxProductCategory, true);
+		comboBoxProductCategory.setBorder(BorderFactory
+				.createLineBorder(ValidationComponentUtils
+						.getMandatoryForeground()));
 	}
 
 }
