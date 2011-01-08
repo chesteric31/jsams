c6fb39a0955ed8fef2c326dd47c3fe027f55ca42
chesteric31 <chesteric31@a5fa9106-e037-4f10-9215-0b9fab2b6631>
2011-01-08 17:13:26 +0000

    Update tableModels.

--- trunk/JSAMS/src/main/java/be/jsams/client/model/table/ProductCategoryTableModel.java
+++ trunk/JSAMS/src/main/java/be/jsams/client/model/table/ProductCategoryTableModel.java
@@ -1,6 +1,5 @@
 package be.jsams.client.model.table;
 
-import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.List;
 
@@ -8,50 +7,46 @@ import javax.swing.table.AbstractTableModel;
 
 import be.jsams.client.i18n.I18nString;
 import be.jsams.client.i18n.JsamsI18nResource;
-import be.jsams.client.i18n.UserContext;
-import be.jsams.server.model.Product;
 import be.jsams.server.model.ProductCategory;
 
 /**
- * {@link AbstractTableModel} for {@link Product} object.
- * 
+ * {@link AbstractTableModel} for {@link ProductCategory} object.
+ *
  * @author chesteric31
- * @version $$Rev$$ $$Date::                  $$ $$Author$$
+ * @version $Rev$ $Date::                  $ $Author$
  */
-public class ProductTableModel extends AbstractTableModel {
+public class ProductCategoryTableModel extends AbstractTableModel {
 
     /**
      * Serial Version UID
      */
-    private static final long serialVersionUID = 5631609209979319706L;
+    private static final long serialVersionUID = -7016107019455922155L;
 
     /**
      * The columns name
      */
-    private List<I18nString> columnNames = Arrays.asList(JsamsI18nResource.COLUMN_ID, JsamsI18nResource.COLUMN_NAME,
-            JsamsI18nResource.COLUMN_PRICE, JsamsI18nResource.COLUMN_STOCK_QUANTITY,
-            JsamsI18nResource.COLUMN_REORDER_LEVEL, JsamsI18nResource.COLUMN_VAT_APPLICABE,
-            JsamsI18nResource.COLUMN_PRODUCT_CATEGORY);
+    private List<I18nString> columnNames = Arrays.asList(JsamsI18nResource.COLUMN_ID,
+            JsamsI18nResource.COLUMN_LABEL_EN, JsamsI18nResource.COLUMN_LABEL_FR, JsamsI18nResource.COLUMN_LABEL_NL);
 
     /**
-     * The list of {@link Product}
+     * The list of {@link ProductCategory}
      */
-    private List<Product> data;
+    private List<ProductCategory> data;
 
     /**
      * 
-     * @return a list of {@link Product}
+     * @return a list of {@link ProductCategory}
      */
-    public List<Product> getData() {
+    public List<ProductCategory> getData() {
         return data;
     }
 
     /**
      * 
      * @param data
-     *            the list of {@link Product} to set
+     *            the list of {@link ProductCategory} to set
      */
-    public void setData(List<Product> data) {
+    public void setData(List<ProductCategory> data) {
         this.data = data;
     }
 
@@ -81,24 +76,11 @@ public class ProductTableModel extends AbstractTableModel {
         case 0:
             return data.get(rowIndex).getId();
         case 1:
-            return data.get(rowIndex).getName();
+            return data.get(rowIndex).getLabel();
         case 2:
-            return data.get(rowIndex).getPrice();
+            return data.get(rowIndex).getLabelFr();
         case 3:
-            return data.get(rowIndex).getQuantityStock();
-        case 4:
-            return data.get(rowIndex).getReorderLevel();
-        case 5:
-            return data.get(rowIndex).getVatApplicable().doubleValue();
-        case 6:
-            ProductCategory category = data.get(rowIndex).getCategory();
-            if (UserContext.isDutch()) {
-                return category.getLabelNl();
-            } else if (UserContext.isFrench()) {
-                return category.getLabelFr();
-            } else {
-                return category.getLabel();
-            }
+            return data.get(rowIndex).getLabelNl();
         default:
             return "";
         }
@@ -126,18 +108,11 @@ public class ProductTableModel extends AbstractTableModel {
         case 1:
             return String.class;
         case 2:
-            return BigDecimal.class;
+            return String.class;
         case 3:
-            return Integer.class;
-        case 4:
-            return Integer.class;
-        case 5:
-            return Double.class;
-        case 6:
             return String.class;
         default:
             return Object.class;
         }
     }
-
 }
