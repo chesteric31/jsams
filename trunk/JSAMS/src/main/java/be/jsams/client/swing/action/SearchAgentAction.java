d264ea4422f3d5e6e94c1f623e49f5bb2303405f
chesteric31 <chesteric31@a5fa9106-e037-4f10-9215-0b9fab2b6631>
2011-01-31 15:21:24 +0000

    Adding search agent action.

---
+++ trunk/JSAMS/src/main/java/be/jsams/client/swing/action/SearchAgentAction.java
@@ -0,0 +1,41 @@
+package be.jsams.client.swing.action;
+
+import java.awt.event.ActionEvent;
+
+import javax.swing.AbstractAction;
+
+import be.jsams.client.context.JsamsApplicationContext;
+import be.jsams.client.i18n.JsamsI18nResource;
+import be.jsams.client.model.panel.SearchAgentPanel;
+import be.jsams.client.swing.component.JsamsDialog;
+import be.jsams.client.swing.listener.AgentTableMouseListener;
+import be.jsams.server.model.Agent;
+
+/**
+ * {@link AbstractAction} to launch {@link SearchAgentPanel}.
+ *
+ * @author chesteric31
+ * @version $Rev$ $Date::                  $ $Author$
+ */
+public class SearchAgentAction extends AbstractAction {
+
+    /**
+     * Serial Version UID
+     */
+    private static final long serialVersionUID = 8266044738535674682L;
+
+    /**
+     * {@inheritDoc}
+     */
+    public void actionPerformed(ActionEvent arg0) {
+        SearchAgentPanel searchAgentPanel = new SearchAgentPanel(new Agent(), new AgentTableMouseListener(),
+                JsamsApplicationContext.getAgentService());
+        JsamsDialog dialog = new JsamsDialog(null, JsamsI18nResource.TITLE_SEARCH_AGENT,
+                "categories/applications-development.png");
+        dialog.add(searchAgentPanel);
+        dialog.pack();
+        dialog.setVisible(true);
+        dialog.setLocationRelativeTo(null);
+    }
+
+}
