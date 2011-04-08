8fd7d2dd10e1383772d9587d1f95005dd27be32a
chesteric31 <chesteric31@a5fa9106-e037-4f10-9215-0b9fab2b6631>
2011-04-08 08:47:38 +0000

    Update junit tests.

--- trunk/JSAMS/src/test/java/be/jsams/server/AllTests.java
+++ trunk/JSAMS/src/test/java/be/jsams/server/AllTests.java
@@ -1,23 +1,25 @@
 package be.jsams.server;
 
-
 import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 import be.jsams.server.dao.impl.AddressDaoImplTest;
+import be.jsams.server.dao.impl.CivilityDaoImplTest;
 import be.jsams.server.dao.impl.CommandDetailDaoImplTest;
+import be.jsams.server.dao.impl.ContactInformationDaoImplTest;
 import be.jsams.server.dao.impl.SocietyDaoImplTest;
 
 /**
+ * Test suite for all junit tests.
  * 
- *
  * @author chesteric31
  * @version $Rev$ $Date::                  $ $Author$
  */
 @RunWith(Suite.class)
-@SuiteClasses(value = { AddressDaoImplTest.class, CommandDetailDaoImplTest.class, SocietyDaoImplTest.class })
+@SuiteClasses(value = { AddressDaoImplTest.class, CivilityDaoImplTest.class, ContactInformationDaoImplTest.class,
+        CommandDetailDaoImplTest.class, SocietyDaoImplTest.class })
 public class AllTests {
 
     /**
--- trunk/JSAMS/src/test/java/be/jsams/server/dao/impl/AddressDaoImplTest.java
+++ trunk/JSAMS/src/test/java/be/jsams/server/dao/impl/AddressDaoImplTest.java
@@ -18,50 +18,50 @@ import be.jsams.server.model.Address;
 
 /**
  * Test class for {@link AddressDaoImpl}.
- *
+ * 
  * @author chesteric31
  * @version $Rev$ $Date::                  $ $Author$
  */
 public class AddressDaoImplTest extends AbstractJUnitTestClass {
 
-	@Autowired
-	private AddressDao dao;
-	
-	private Address address;
-	
-	@Before
-	public void setUp() {
-		address = new Address();
-		address.setCity("Paris");
-		address.setCountry("France");
-		address.setNumber("79");
-		address.setStreet("Rue de Varenne");
-		address.setZipCode("75007");
-	}
+    @Autowired
+    private AddressDao dao;
 
-	@Test
-	public void testAdd() {
-		dao.add(address);
-		assertNotNull(address.getId());
-	}
-	
-	@Test(expected=EntityExistsException.class)
-	public void testAddNullCity() {
-		address.setCity(null);
-		dao.add(address);
-	}
-	
-	@Test(expected=EntityExistsException.class)
-	public void testAddNullCountry() {
-		address.setCountry(null);
-		dao.add(address);
-	}
+    private Address address;
 
-	@Test(expected=EntityExistsException.class)
-	public void testAddNullNumber() {
-		address.setNumber(null);
-		dao.add(address);
-	}
+    @Before
+    public void setUp() {
+        address = new Address();
+        address.setCity("Paris");
+        address.setCountry("France");
+        address.setNumber("79");
+        address.setStreet("Rue de Varenne");
+        address.setZipCode("75007");
+    }
+
+    @Test
+    public void testAdd() {
+        dao.add(address);
+        assertNotNull(address.getId());
+    }
+
+    @Test(expected = EntityExistsException.class)
+    public void testAddNullCity() {
+        address.setCity(null);
+        dao.add(address);
+    }
+
+    @Test(expected = EntityExistsException.class)
+    public void testAddNullCountry() {
+        address.setCountry(null);
+        dao.add(address);
+    }
+
+    @Test(expected = EntityExistsException.class)
+    public void testAddNullNumber() {
+        address.setNumber(null);
+        dao.add(address);
+    }
 
     @Test(expected = EntityExistsException.class)
     public void testAddNullStreet() {
@@ -69,15 +69,15 @@ public class AddressDaoImplTest extends AbstractJUnitTestClass {
         dao.add(address);
     }
 
-	@Test
-	public void testFindAll() {
-	    int sizeBefore = dao.findAll().size();
-		dao.add(address);
-		Address anOtherAddress = new Address();
-		anOtherAddress.setCity("Brussels");
-		anOtherAddress.setCountry("Belgium");
-		anOtherAddress.setNumber("1");
-		anOtherAddress.setStreet("Rue Neuve");
+    @Test
+    public void testFindAll() {
+        int sizeBefore = dao.findAll().size();
+        dao.add(address);
+        Address anOtherAddress = new Address();
+        anOtherAddress.setCity("Brussels");
+        anOtherAddress.setCountry("Belgium");
+        anOtherAddress.setNumber("1");
+        anOtherAddress.setStreet("Rue Neuve");
         anOtherAddress.setZipCode("1000");
         dao.add(anOtherAddress);
         List<Address> addresses = dao.findAll();
@@ -86,30 +86,31 @@ public class AddressDaoImplTest extends AbstractJUnitTestClass {
         } else {
             assertTrue(addresses.size() == 2);
         }
-	}
+    }
 
-	@Test
-	public void testFindById() {
-		dao.add(address);
-		Address findAddress = dao.findById(address.getId());
-		assertNotNull(findAddress);
-	}
+    @Test
+    public void testFindById() {
+        dao.add(address);
+        Address foundAddress = dao.findById(address.getId());
+        assertNotNull(foundAddress);
+    }
 
-	@Test
-	public void testRemove() {
-		dao.add(address);
-		dao.delete(address);
-		Address findAddress = dao.findById(address.getId());
-		assertNull(findAddress);
-	}
+    @Test
+    public void testDelete() {
+        dao.add(address);
+        dao.delete(address);
+        Address foundAddress = dao.findById(address.getId());
+        assertNull(foundAddress);
+    }
 
-	@Test
-	public void testUpdate() {
-		dao.add(address);
-		address.setNumber("101");
-		dao.update(address);
-		Address findAddress = dao.findById(address.getId());
-		assertTrue(findAddress.getNumber().equals("101"));
-	}
+    @Test
+    public void testUpdate() {
+        dao.add(address);
+        String fakeNumber = "101";
+        address.setNumber(fakeNumber);
+        dao.update(address);
+        Address foundAddress = dao.findById(address.getId());
+        assertTrue(foundAddress.getNumber().equals(fakeNumber));
+    }
 
 }
