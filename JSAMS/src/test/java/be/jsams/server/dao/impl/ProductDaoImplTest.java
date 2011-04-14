81025471fe644e194104af91c7c21a3295f75bcd
chesteric31 <chesteric31@users.noreply.github.com>
2011-04-14 14:11:03 +0000

    SVN keywords setting.

---
+++ JSAMS/src/test/java/be/jsams/server/dao/impl/ProductDaoImplTest.java
@@ -0,0 +1,64 @@
+package be.jsams.server.dao.impl;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Before;
+import org.junit.Test;
+import be.jsams.server.dao.AbstractJUnitTestClass;
+
+/**
+ * 
+ *
+ * @author chesteric31
+ * @version $Rev$ $Date::                  $ $Author$
+ */
+public class ProductDaoImplTest extends AbstractJUnitTestClass {
+
+    /**
+     * @throws java.lang.Exception
+     */
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    /**
+     * Test method for {@link be.jsams.server.dao.impl.DaoImpl#add(java.lang.Object)}.
+     */
+    @Test
+    public void testAdd() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link be.jsams.server.dao.impl.DaoImpl#findAll()}.
+     */
+    @Test
+    public void testFindAll() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link be.jsams.server.dao.impl.DaoImpl#findById(java.lang.Long)}.
+     */
+    @Test
+    public void testFindById() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link be.jsams.server.dao.impl.DaoImpl#delete(java.lang.Object)}.
+     */
+    @Test
+    public void testDeleteT() {
+        fail("Not yet implemented");
+    }
+
+    /**
+     * Test method for {@link be.jsams.server.dao.impl.DaoImpl#update(java.lang.Object)}.
+     */
+    @Test
+    public void testUpdate() {
+        fail("Not yet implemented");
+    }
+
+}
