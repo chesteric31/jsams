e341b5073f0534f17069c858134ea8cf6b321b82
chesteric31 <chesteric31@a5fa9106-e037-4f10-9215-0b9fab2b6631>
2011-04-15 09:54:08 +0000

    Set SVN properties.

--- trunk/JSAMS/src/main/java/be/jsams/client/desktop/AbstractMenuBuilder.java
+++ trunk/JSAMS/src/main/java/be/jsams/client/desktop/AbstractMenuBuilder.java
@@ -6,7 +6,7 @@ import be.jsams.client.swing.component.JsamsMenu;
  * Abstract class to build menu.
  *
  * @author chesteric31
- * @version $Rev$ $Date::                  $ $Author$
+ * @version $Rev: 692 $ $Date::                  $ $Author$
  */
 public abstract class AbstractMenuBuilder {
 
--- trunk/JSAMS/src/main/java/be/jsams/client/desktop/JsamsFileMenuBuilder.java
+++ trunk/JSAMS/src/main/java/be/jsams/client/desktop/JsamsFileMenuBuilder.java
@@ -17,7 +17,7 @@ import be.jsams.client.swing.component.JsamsMenuItem;
  * Specific menu builder for file menu.
  *
  * @author chesteric31
- * @version $Rev$ $Date::                  $ $Author$
+ * @version $Rev: 692 $ $Date::                  $ $Author$
  */
 public class JsamsFileMenuBuilder extends AbstractMenuBuilder {
 
