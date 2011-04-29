86b241dad50259e378bd0f619e885d59d9f9de00
chesteric31 <chesteric31@a5fa9106-e037-4f10-9215-0b9fab2b6631>
2011-04-29 08:26:00 +0000

    Update package.

--- trunk/JSAMS/src/main/java/be/jsams/common/bean/view/sale/CommandBeanView.java
+++ trunk/JSAMS/src/main/java/be/jsams/common/bean/view/sale/CommandBeanView.java
@@ -53,7 +53,7 @@ import com.toedter.calendar.JDateChooser;
  * Implementation of all sorts of views for {@link CommandBean} object.
  * 
  * @author chesteric31
- * @version $Rev$ $Date:: $ $Author$
+ * @version $$Rev$$ $$Date::                  $$ $$Author$$
  */
 public class CommandBeanView extends AbstractView<CommandBean, JPanel, JPanel> {
 
