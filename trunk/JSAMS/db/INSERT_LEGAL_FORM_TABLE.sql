34c8f66bb6b64f75b10a56e88de128f22d937045
chesteric31 <chesteric31@a5fa9106-e037-4f10-9215-0b9fab2b6631>
2011-05-12 06:42:23 +0000

    Update legal forms dutch translations.

--- trunk/JSAMS/db/INSERT_LEGAL_FORM_TABLE.sql
+++ trunk/JSAMS/db/INSERT_LEGAL_FORM_TABLE.sql
@@ -1,15 +1,15 @@
 INSERT INTO BTS.LEGAL_FORM (LABEL, LABEL_FR, LABEL_NL)
-VALUES ("Public Limited Company", "Entreprise individuelle", "Naamloze Vennootschap (NV)");
+VALUES ("Public Limited Company", "Entreprise individuelle", "Eenmanszaak");
 INSERT INTO BTS.LEGAL_FORM (LABEL, LABEL_FR, LABEL_NL)
-VALUES ("Limited Company", "Société Privée à Responsabilité Limitée (Unipersonnelle) (SPRL)", "Personenvennootschap met Beperkte Aansprakelijkheid (PVBA)");
+VALUES ("Limited Company", "Société Privée à Responsabilité Limitée (Unipersonnelle) (SPRL)", "Besloten Vennootschap met Veperkte Aansprakelijkheid (BVBA)");
 INSERT INTO BTS.LEGAL_FORM (LABEL, LABEL_FR, LABEL_NL)
 VALUES ("Limited Company Starter", "SPRL Starter (SPRL-S)", "Personenvennootschap met Beperkte Aansprakelijkheid (PVBA Starter)");
 INSERT INTO BTS.LEGAL_FORM (LABEL, LABEL_FR, LABEL_NL)
-VALUES ("Private Limited Company (LTD)", "Société Anonyme (SA)", "Besloten Vennootschap met Beperkte Aansprakelijkheid (BVBA)");
+VALUES ("Private Limited Company (LTD)", "Société Anonyme (SA)", "Naamloze vennootschap (NV)");
 INSERT INTO BTS.LEGAL_FORM (LABEL, LABEL_FR, LABEL_NL)
 VALUES ("Partnership Limited By Shares", "Société en Commandite par Actions (SCA)", "Commanditaire Vennootschap op Aandelen (CVA)");
 INSERT INTO BTS.LEGAL_FORM (LABEL, LABEL_FR, LABEL_NL)
-VALUES ("Limited Liability Partnership (LLP)", "Société Coopérative à Responsabilité Limitée (SCRL)", "LLP");
+VALUES ("Limited Liability Partnership (LLP)", "Société Coopérative à Responsabilité Limitée (SCRL)", "Coöperatieve Vennootschap met Beperkte Aansprakelijkheid (CVBA)");
 INSERT INTO BTS.LEGAL_FORM (LABEL, LABEL_FR, LABEL_NL)
 VALUES ("Illimited Liability Partnership", "Société Coopérative à Responsabilité Illimitée (SCRI)", "Coöperatieve Vennootschap met Onbeperkte en Hoofdelijke Aansprakelijkheid (CVOA)");
 INSERT INTO BTS.LEGAL_FORM (LABEL, LABEL_FR, LABEL_NL)
