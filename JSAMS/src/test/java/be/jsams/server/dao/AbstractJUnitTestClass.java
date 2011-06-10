54716f8aa8812fbf562fa8cbeb85c8c189acfe40
chesteric31 <chesteric31@users.noreply.github.com>
2011-06-10 14:33:15 +0000

    Remove the @Transactional annotation redundant with the super class.

--- JSAMS/src/test/java/be/jsams/server/dao/AbstractJUnitTestClass.java
+++ JSAMS/src/test/java/be/jsams/server/dao/AbstractJUnitTestClass.java
@@ -3,7 +3,6 @@ package be.jsams.server.dao;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
 import org.springframework.test.context.transaction.TransactionConfiguration;
-import org.springframework.transaction.annotation.Transactional;
 
 /**
  * Abstract class for all test classes.
@@ -13,7 +12,6 @@ import org.springframework.transaction.annotation.Transactional;
  */
 @ContextConfiguration(locations = "classpath:ApplicationContext.xml")
 @TransactionConfiguration(transactionManager = "txManager", defaultRollback = true)
-@Transactional
 public abstract class AbstractJUnitTestClass extends
 		AbstractTransactionalJUnit4SpringContextTests {
 
