25df152e6487de020654d31e39673eabb07ba6bb
chesteric31 <chesteric31@users.noreply.github.com>
2011-07-27 16:39:15 +0000

    Adding new table listener for wizard.

---
+++ JSAMS/src/main/java/be/jsams/client/swing/listener/EstimateWizardTableMouseListener.java
@@ -0,0 +1,82 @@
+package be.jsams.client.swing.listener;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.ArrayList;
+import java.util.List;
+
+import be.jsams.client.model.table.EstimateTableModel;
+import be.jsams.client.swing.component.JsamsTable;
+import be.jsams.common.bean.model.sale.EstimateBean;
+import be.jsams.common.bean.model.transfer.TransferBean;
+
+/**
+ * 
+ *
+ * @author chesteric31
+ * @version $Revision$ $Date::                  $ $Author$
+ */
+public class EstimateWizardTableMouseListener implements MouseListener {
+
+    private TransferBean bean;
+
+    /**
+     * Constructor
+     * 
+     * @param bean the {@link TransferBean}
+     */
+    public EstimateWizardTableMouseListener(TransferBean bean) {
+        this.bean = bean;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        JsamsTable table = (JsamsTable) e.getSource();
+        if (e.getClickCount() == 1) {
+            int selectedRow = table.getSelectedRow();
+            if (selectedRow > -1) {
+                int selectedRowModel = table.convertRowIndexToModel(selectedRow);
+                EstimateTableModel model = (EstimateTableModel) table.getModel();
+                List<EstimateBean> beans = new ArrayList<EstimateBean>();
+                beans.add(model.getRow(selectedRowModel));
+                bean.setDocuments(beans);
+            }
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void mousePressed(MouseEvent e) {
+        // TODO Auto-generated method stub
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        // TODO Auto-generated method stub
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        // TODO Auto-generated method stub
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void mouseExited(MouseEvent e) {
+        // TODO Auto-generated method stub
+    }
+
+}
