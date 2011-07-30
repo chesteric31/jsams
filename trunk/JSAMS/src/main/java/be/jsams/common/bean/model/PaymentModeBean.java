327b166f64ffad3f44fb0ad22942404f5b1b6fa6
chesteric31 <chesteric31@a5fa9106-e037-4f10-9215-0b9fab2b6631>
2011-07-30 14:33:39 +0000

    Adding into payment mode: days number, end month and additional days.

--- trunk/JSAMS/src/main/java/be/jsams/common/bean/model/PaymentModeBean.java
+++ trunk/JSAMS/src/main/java/be/jsams/common/bean/model/PaymentModeBean.java
@@ -17,6 +17,14 @@ public class PaymentModeBean extends AbstractTranslatableIdentityBean<PaymentMod
      * Serial Version UID
      */
     private static final long serialVersionUID = 3641238456200690731L;
+    
+    private Integer daysNumber;
+    private boolean monthEnd;
+    private Integer additionalDays;
+
+    private static final String DAYS_NUMBER_PROPERTY = "daysNumber";
+    private static final String MONTH_END_PROPERTY = "monthEnd";
+    private static final String ADDITIONAL_DAYS_PROPERTY = "additionalDays";
 
     /**
      * Constructor.
@@ -25,6 +33,9 @@ public class PaymentModeBean extends AbstractTranslatableIdentityBean<PaymentMod
      */
     public PaymentModeBean(PaymentMode model) {
         super(model);
+        this.daysNumber = model.getAdditionalDays();
+        this.monthEnd = model.isMonthEnd();
+        this.additionalDays = model.getAdditionalDays();
     }
 
     /**
@@ -51,11 +62,81 @@ public class PaymentModeBean extends AbstractTranslatableIdentityBean<PaymentMod
     }
 
     /**
+     * @return the daysNumber
+     */
+    public Integer getDaysNumber() {
+        return daysNumber;
+    }
+
+    /**
+     * @param daysNumber the daysNumber to set
+     */
+    public void setDaysNumber(Integer daysNumber) {
+        Integer oldValue = this.daysNumber;
+        this.daysNumber = daysNumber;
+        firePropertyChange(DAYS_NUMBER_PROPERTY, oldValue, this.daysNumber);
+    }
+
+    /**
+     * @return the monthEnd
+     */
+    public boolean isMonthEnd() {
+        return monthEnd;
+    }
+
+    /**
+     * @param monthEnd the monthEnd to set
+     */
+    public void setMonthEnd(boolean monthEnd) {
+        boolean oldValue = this.monthEnd;
+        this.monthEnd = monthEnd;
+        firePropertyChange(MONTH_END_PROPERTY, oldValue, this.monthEnd);
+    }
+    /**
+     * @return the additionalDays
+     */
+    public Integer getAdditionalDays() {
+        return additionalDays;
+    }
+
+    /**
+     * @param additionalDays the additionalDays to set
+     */
+    public void setAdditionalDays(Integer additionalDays) {
+        Integer oldValue = this.additionalDays;
+        this.additionalDays = additionalDays;
+        firePropertyChange(ADDITIONAL_DAYS_PROPERTY, oldValue, this.additionalDays);
+    }
+
+
+    /**
      * {@inheritDoc}
      */
     @Override
     public PaymentModeBeanView getView() {
         return new PaymentModeBeanView(this);
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    public void clear() {
+        super.clear();
+        setDaysNumber(null);
+        setMonthEnd(false);
+        setAdditionalDays(null);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void refresh(AbstractIdentityBean<?, ?> bean) {
+        PaymentModeBean other = (PaymentModeBean) bean;
+        super.refresh(bean);
+        setDaysNumber(other.getDaysNumber());
+        setMonthEnd(other.isMonthEnd());
+        setAdditionalDays(other.getAdditionalDays());
     }
 
 }
--- trunk/JSAMS/src/main/java/be/jsams/server/model/PaymentMode.java
+++ trunk/JSAMS/src/main/java/be/jsams/server/model/PaymentMode.java
@@ -1,5 +1,6 @@
 package be.jsams.server.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
@@ -15,6 +16,10 @@ import be.jsams.common.bean.model.PaymentModeBean;
 @Table(name = "PAYMENT_MODE")
 public class PaymentMode extends AbstractTranslatableIdentity {
 
+    private Integer daysNumber;
+    private boolean monthEnd;
+    private Integer additionalDays;
+    
     /**
      * Constructor.
      */
@@ -29,6 +34,53 @@ public class PaymentMode extends AbstractTranslatableIdentity {
      */
     public PaymentMode(PaymentModeBean bean) {
         super(bean);
+        this.daysNumber = bean.getDaysNumber();
+        this.monthEnd = bean.isMonthEnd();
+        this.additionalDays = bean.getAdditionalDays();
     }
 
+    /**
+     * @return the daysNumber
+     */
+    @Column(name = "DAYS_NUMBER")
+    public Integer getDaysNumber() {
+        return daysNumber;
+    }
+
+    /**
+     * @param daysNumber the daysNumber to set
+     */
+    public void setDaysNumber(Integer daysNumber) {
+        this.daysNumber = daysNumber;
+    }
+
+    /**
+     * @return the monthEnd
+     */
+    @Column(name = "MONTH_END")
+    public boolean isMonthEnd() {
+        return monthEnd;
+    }
+
+    /**
+     * @param monthEnd the monthEnd to set
+     */
+    public void setMonthEnd(boolean monthEnd) {
+        this.monthEnd = monthEnd;
+    }
+    /**
+     * @return the additionalDays
+     */
+    @Column(name = "ADDITIONAL_DAYS")
+    public Integer getAdditionalDays() {
+        return additionalDays;
+    }
+
+    /**
+     * @param additionalDays the additionalDays to set
+     */
+    public void setAdditionalDays(Integer additionalDays) {
+        this.additionalDays = additionalDays;
+    }
+    
 }
