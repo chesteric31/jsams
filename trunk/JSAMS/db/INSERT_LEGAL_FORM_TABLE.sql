93a349cb6b20881f38f74de97ec81b4f06d663bd
chesteric31 <chesteric31@a5fa9106-e037-4f10-9215-0b9fab2b6631>
2011-10-24 18:29:37 +0000

    accent problem.

--- trunk/JSAMS/db/INSERT_LEGAL_FORM_TABLE.sql
+++ trunk/JSAMS/db/INSERT_LEGAL_FORM_TABLE.sql
@@ -1,18 +1,18 @@
 INSERT INTO BTS.LEGAL_FORM (LABEL, LABEL_FR, LABEL_NL)
 VALUES ("Public Limited Company", "Entreprise individuelle", "Eenmanszaak");
 INSERT INTO BTS.LEGAL_FORM (LABEL, LABEL_FR, LABEL_NL)
-VALUES ("Limited Company", "Société Privée à Responsabilité Limitée (Unipersonnelle) (SPRL)", "Besloten Vennootschap met Veperkte Aansprakelijkheid (BVBA)");
+VALUES ("Limited Company", "SociÃ©tÃ© PrivÃ©e Ã  ResponsabilitÃ© LimitÃ©e (Unipersonnelle) (SPRL)", "Besloten Vennootschap met Veperkte Aansprakelijkheid (BVBA)");
 INSERT INTO BTS.LEGAL_FORM (LABEL, LABEL_FR, LABEL_NL)
 VALUES ("Limited Company Starter", "SPRL Starter (SPRL-S)", "Personenvennootschap met Beperkte Aansprakelijkheid (PVBA Starter)");
 INSERT INTO BTS.LEGAL_FORM (LABEL, LABEL_FR, LABEL_NL)
-VALUES ("Private Limited Company (LTD)", "Société Anonyme (SA)", "Naamloze vennootschap (NV)");
+VALUES ("Private Limited Company (LTD)", "SociÃ©tÃ© Anonyme (SA)", "Naamloze vennootschap (NV)");
 INSERT INTO BTS.LEGAL_FORM (LABEL, LABEL_FR, LABEL_NL)
-VALUES ("Partnership Limited By Shares", "Société en Commandite par Actions (SCA)", "Commanditaire Vennootschap op Aandelen (CVA)");
+VALUES ("Partnership Limited By Shares", "SociÃ©tÃ© en Commandite par Actions (SCA)", "Commanditaire Vennootschap op Aandelen (CVA)");
 INSERT INTO BTS.LEGAL_FORM (LABEL, LABEL_FR, LABEL_NL)
-VALUES ("Limited Liability Partnership (LLP)", "Société Coopérative à Responsabilité Limitée (SCRL)", "Coöperatieve Vennootschap met Beperkte Aansprakelijkheid (CVBA)");
+VALUES ("Limited Liability Partnership (LLP)", "SociÃ©tÃ© CoopÃ©rative Ã  ResponsabilitÃ© LimitÃ©e (SCRL)", "CoÃ¶peratieve Vennootschap met Beperkte Aansprakelijkheid (CVBA)");
 INSERT INTO BTS.LEGAL_FORM (LABEL, LABEL_FR, LABEL_NL)
-VALUES ("Illimited Liability Partnership", "Société Coopérative à Responsabilité Illimitée (SCRI)", "Coöperatieve Vennootschap met Onbeperkte en Hoofdelijke Aansprakelijkheid (CVOA)");
+VALUES ("Illimited Liability Partnership", "SociÃ©tÃ© CoopÃ©rative Ã  ResponsabilitÃ© IllimitÃ©e (SCRI)", "CoÃ¶peratieve Vennootschap met Onbeperkte en Hoofdelijke Aansprakelijkheid (CVOA)");
 INSERT INTO BTS.LEGAL_FORM (LABEL, LABEL_FR, LABEL_NL)
-VALUES ("General Partnership", "Société en Nom Collectif (SNC)", "Vennootschap onder Firma (VOF)");
+VALUES ("General Partnership", "SociÃ©tÃ© en Nom Collectif (SNC)", "Vennootschap onder Firma (VOF)");
 INSERT INTO BTS.LEGAL_FORM (LABEL, LABEL_FR, LABEL_NL)
-VALUES ("Limited Partnership", "Société en Commandite Simple (SCS)", "Commanditaire Vennootschap (CV)");
+VALUES ("Limited Partnership", "SociÃ©tÃ© en Commandite Simple (SCS)", "Commanditaire Vennootschap (CV)");
