059a667f2a3f6bc1ebccc9b042a170e2d7c7f129
chesteric31 <chesteric31@a5fa9106-e037-4f10-9215-0b9fab2b6631>
2011-12-22 19:13:01 +0000

    Adding about dialog.

--- trunk/JSAMS/src/main/java/be/jsams/client/desktop/JsamsHelpMenuBuilder.java
+++ trunk/JSAMS/src/main/java/be/jsams/client/desktop/JsamsHelpMenuBuilder.java
@@ -1,8 +1,14 @@
 package be.jsams.client.desktop;
 
+import java.awt.event.ActionEvent;
+
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.Icon;
 import javax.swing.JSeparator;
 
 import be.jsams.client.i18n.JsamsI18nResource;
+import be.jsams.client.model.dialog.AboutDialog;
 import be.jsams.client.swing.component.JsamsMenu;
 import be.jsams.client.swing.component.JsamsMenuItem;
 
@@ -28,8 +34,33 @@ public class JsamsHelpMenuBuilder extends AbstractMenuBuilder {
         helpMenu.add(helpMI);
         helpMenu.add(new JSeparator());
         aboutMI = new JsamsMenuItem(JsamsI18nResource.MENU_ITEM_ABOUT, "categories/applications-office.png");
+        aboutMI.setAction(aboutAction(aboutMI.getText(), aboutMI.getIcon()));
         helpMenu.add(aboutMI);
         return helpMenu;
+    }
+
+    /**
+     * {@link AbstractAction} to open the about dialog.
+     * 
+     * @param text the text to display
+     * @param icon the {@link Icon} to display
+     * @return an {@link Action} to open the about dialog
+     */
+    private Action aboutAction(String text, Icon icon) {
+        AbstractAction action = new AbstractAction() {
+
+            /**
+             * Serial Version UID
+             */
+            private static final long serialVersionUID = 6621546191772229680L;
+
+            public void actionPerformed(ActionEvent event) {
+                new AboutDialog(JsamsDesktop.getInstance().getMainWindow(), JsamsI18nResource.TITLE_ABOUT);
+            }
+        };
+        action.putValue(Action.NAME, text);
+        action.putValue(Action.SMALL_ICON, icon);
+        return action;
     }
 
 }
