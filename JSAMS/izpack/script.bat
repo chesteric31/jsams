b28bb1e019c0a970d4abc077c7663b3c849233f9
chesteric31 <chesteric31@users.noreply.github.com>
2011-12-24 16:05:26 +0000

    Correction db scripts.

--- JSAMS/izpack/script.bat
+++ JSAMS/izpack/script.bat
@@ -13,5 +13,5 @@ echo "Create database"
 echo "Reload MySQL server"
 "$INSTALL_PATH\mysql-5.1.60-win32\bin\mysqladmin.exe" --user=root --password=$DB_PASSWORD reload
 echo "Execute db.sql"
-"$INSTALL_PATH\mysql-5.1.60-win32\bin\mysql.exe" --user=root --password=$DB_PASSWORD --database=$DB_SCHEMA < "$INSTALL_PATH\db\db_win.sql"
+"$INSTALL_PATH\mysql-5.1.60-win32\bin\mysql.exe" --user=root --password=$DB_PASSWORD --database=$DB_SCHEMA < "$INSTALL_PATH\scripts\db_win.sql"
 echo "END Execution of SQL Queries"
