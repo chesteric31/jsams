2552229ec4c8a947fa3b5a32c244c1d126888018
chesteric31 <chesteric31@users.noreply.github.com>
2012-02-26 16:52:42 +0000

    Adding updater for 1.0.1 to 1.0.2

--- JSAMS/izpack/win/create_db.bat
+++
@@ -1,7 +0,0 @@
-@ECHO OFF
-echo "Change root password"
-"$INSTALL_PATH\win\mysql-5.1.60-win32\bin\mysqladmin.exe" --user=root password $DB_PASSWORD
-echo "Create database"
-"$INSTALL_PATH\win\mysql-5.1.60-win32\bin\mysqladmin.exe" --user=root --password=$DB_PASSWORD CREATE $DB_SCHEMA
-echo "Reload MySQL server"
-"$INSTALL_PATH\win\mysql-5.1.60-win32\bin\mysqladmin.exe" --user=root --password=$DB_PASSWORD reload
