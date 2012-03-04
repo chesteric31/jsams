15022e6a141b6485b3aa07866725da51a9e3490d
chesteric31 <chesteric31@users.noreply.github.com>
2012-03-04 20:46:31 +0000

    Adding support to update 1.0.1 directory to 1.0.2 layout.

--- JSAMS/izpack/update/win/1.0.2/update_db_scripts.bat
+++ JSAMS/izpack/update/win/1.0.2/update_db_scripts.bat
@@ -1,4 +1,14 @@
 @ECHO OFF
-echo "Execute db.sql"
+echo "stop the mysql service"
 Rem for version 1.0.2 and after it must be with win between $INSTALL_PATH and mysql
-"$INSTALL_PATH\mysql-5.1.60-win32\bin\mysql.exe" --user=root --password=$DB_PASSWORD --database=$DB_SCHEMA < "$INSTALL_PATH\scripts\1.0.2\ALTER_TABLES_F_1_0_1_T_1_0_2.sql"
+$INSTALL_PATH\mysql-5.1.60-win32\bin\mysqld.exe" --uninstall
+net stop MySQL
+sc delete MySQL
+echo "move mysql dir to new dir: win\mysql"
+move "$INSTALL_PATH\mysql-5.1.60-win32" "$INSTALL_PATH\win\mysql-5.1.60-win32"
+$INSTALL_PATH\win\mysql-5.1.60-win32\bin\mysqld.exe" --install
+net stop MySQL
+"$INSTALL_PATH\win\mysql-5.1.60-win32\bin\mysql.exe" --user=root --password=$DB_PASSWORD --database=$DB_SCHEMA < "$INSTALL_PATH\scripts\1.0.2\ALTER_TABLES_F_1_0_1_T_1_0_2.sql"
+echo "delete all 1.0.1 files"
+del "$INSTALL_PATH\script.bat"
+del "$INSTALL_PATH\JSAMS-1.0.1-SNAPSHOT.jar"
