6e4ad6c6137ad4c81b8558914c1d9981bb3833a6
chesteric31 <chesteric31@users.noreply.github.com>
2012-03-11 20:47:25 +0000

    Adding jsamsConstants.

---
+++ JSAMS/src/main/java/be/jsams/common/util/JsamsConstants.java
@@ -0,0 +1,26 @@
+package be.jsams.common.util;
+
+/**
+ * All constants of the application.
+ *
+ * @author chesteric31
+ * @version $Revision$ $Date::                  $ $Author$
+ */
+public final class JsamsConstants {
+    
+    /**
+     * Constructor.
+     */
+    private JsamsConstants() {
+        
+    }
+
+    public static final String PROXY_TO_SET = "proxyToSet";
+    public static final String PROXY_HOST = "proxyHost";
+    public static final String PROXY_PORT = "proxyPort";
+    public static final String PROXY_TO_AUTHENTICATE = "proxyToAuthenticate";
+    public static final String PROXY_USER = "proxyUser";
+    public static final String PROXY_PASS = "proxyPass";
+    public static final String PACKAGE_ROOT_NAME = "be.jsams";
+
+}
