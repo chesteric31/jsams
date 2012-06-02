cea975e13681ba318505ada547c5d3fc7c19982d
chesteric31 <chesteric31@users.noreply.github.com>
2012-06-02 13:39:39 +0000

    Remove warning for hashcode method.

--- JSAMS/src/main/java/be/jsams/common/bean/model/AbstractIdentityBean.java
+++ JSAMS/src/main/java/be/jsams/common/bean/model/AbstractIdentityBean.java
@@ -109,7 +109,12 @@ public abstract class AbstractIdentityBean<M extends AbstractIdentity, V extends
     public int hashCode() {
         final int prime = 31;
         int result = 1;
-        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result;
+        if (id == null) {
+            result += 0;
+        } else {
+            result += id.hashCode();
+        }
         return result;
     }
 
