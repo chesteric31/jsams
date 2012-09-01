19f2cd7c0027ecca199882370e57c4729f2b61ed
chesteric31 <chesteric31@users.noreply.github.com>
2012-09-01 11:42:51 +0000

    Issue 17: 	Addings and corrections of JUnits test: Correct of DownloaderServiceImplTest.

--- JSAMS/src/test/java/be/jsams/server/service/update/impl/DownloaderServiceImplTest.java
+++ JSAMS/src/test/java/be/jsams/server/service/update/impl/DownloaderServiceImplTest.java
@@ -1,7 +1,10 @@
 package be.jsams.server.service.update.impl;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.fail;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 import org.junit.Test;
@@ -43,19 +46,29 @@ public class DownloaderServiceImplTest {
      * Test method for
      * {@link be.jsams.server.service.update.impl.DownloaderServiceImpl#retrieveHostStringForUpdates()}
      * .
+     * @throws IOException 
      */
     @Test
-    public void testRetrieveHostStringForUpdates() {
+    public void testRetrieveHostStringForUpdates() throws IOException {
         DownloaderServiceImpl downloaderService = new DownloaderServiceImpl();
         PropertyHolder propertyHolder = new PropertyHolderImpl();
         downloaderService.setPropertyHolder(propertyHolder);
-        RSSFeedParser rssFeedParser = new RSSFeedParserImpl(
-                "file:///home//chesteric31//Dev//workspace-jsams//JSAMS//updates.rss");
+        String os = System.getProperty("os.name").toLowerCase();
+        String rssFileName = null;
+        // windows
+        if (os.indexOf("win") >= 0) {
+            rssFileName = "file:\\";
+        } else if ((os.indexOf("mac") >= 0) || (os.indexOf("nix") >= 0 || os.indexOf("nux") >= 0)) {
+            rssFileName = "file://";
+        }
+        File path = new File(".");
+        String separator = File.separator;
+        rssFileName += path.getCanonicalPath() + separator + "src" + separator + "test" + separator
+                + "resources" + separator + "Updates.rss";
+        RSSFeedParser rssFeedParser = new RSSFeedParserImpl(rssFileName);
         downloaderService.setRssFeedParser(rssFeedParser);
         List<String> stringForUpdates = downloaderService.retrieveHostStringForUpdates();
-        for (String string : stringForUpdates) {
-            System.out.println(string);
-        }
+        assertFalse(stringForUpdates.isEmpty());
     }
 
 }
