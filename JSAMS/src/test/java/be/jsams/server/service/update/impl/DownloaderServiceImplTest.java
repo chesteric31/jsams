3cfbdf41f49075afcc69d8b4a684d88b26d43a71
chesteric31 <chesteric31@users.noreply.github.com>
2012-09-17 18:08:38 +0000

    Issue 17: 	Addings and corrections of JUnits test: correction of downloaderserviceimpltest.

--- JSAMS/src/test/java/be/jsams/server/service/update/impl/DownloaderServiceImplTest.java
+++ JSAMS/src/test/java/be/jsams/server/service/update/impl/DownloaderServiceImplTest.java
@@ -1,12 +1,13 @@
 package be.jsams.server.service.update.impl;
 
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.fail;
+import static org.junit.Assert.assertNotNull;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import be.jsams.server.service.property.PropertyHolder;
@@ -22,35 +23,14 @@ import be.jsams.server.service.rss.impl.RSSFeedParserImpl;
  */
 public class DownloaderServiceImplTest {
 
-    /**
-     * Test method for
-     * {@link be.jsams.server.service.update.impl.DownloaderServiceImpl#retrieveAvailableUpdateVersion()}
-     * .
-     */
-    @Test
-    public void testRetrieveAvailableUpdateVersion() {
-        fail("Not yet implemented");
-    }
+    private DownloaderServiceImpl downloaderService;
 
     /**
-     * Test method for
-     * {@link be.jsams.server.service.update.impl.DownloaderServiceImpl#downloadAvailableUpdateFile(java.lang.String)}
-     * .
+     * @throws java.lang.Exception a possible {@link Exception}
      */
-    @Test
-    public void testRetrieveAvailableUpdateFile() {
-        fail("Not yet implemented");
-    }
-
-    /**
-     * Test method for
-     * {@link be.jsams.server.service.update.impl.DownloaderServiceImpl#retrieveHostStringForUpdates()}
-     * .
-     * @throws IOException 
-     */
-    @Test
-    public void testRetrieveHostStringForUpdates() throws IOException {
-        DownloaderServiceImpl downloaderService = new DownloaderServiceImpl();
+    @Before
+    public void setUp() throws Exception {
+        downloaderService = new DownloaderServiceImpl();
         PropertyHolder propertyHolder = new PropertyHolderImpl();
         downloaderService.setPropertyHolder(propertyHolder);
         String os = System.getProperty("os.name").toLowerCase();
@@ -63,10 +43,43 @@ public class DownloaderServiceImplTest {
         }
         File path = new File(".");
         String separator = File.separator;
-        rssFileName += path.getCanonicalPath() + separator + "src" + separator + "test" + separator
-                + "resources" + separator + "Updates.rss";
+        rssFileName += path.getCanonicalPath() + separator + "src" + separator + "test" + separator + "resources"
+                + separator + "Updates.rss";
         RSSFeedParser rssFeedParser = new RSSFeedParserImpl(rssFileName);
         downloaderService.setRssFeedParser(rssFeedParser);
+    }
+
+    /**
+     * Test method for
+     * {@link be.jsams.server.service.update.impl.DownloaderServiceImpl#retrieveAvailableUpdateVersion()}
+     * .
+     */
+    @Test
+    public void testRetrieveAvailableUpdateVersion() {
+        String availableUpdateVersion = downloaderService.retrieveAvailableUpdateVersion();
+        assertNotNull(availableUpdateVersion);
+    }
+
+    /**
+     * Test method for
+     * {@link be.jsams.server.service.update.impl.DownloaderServiceImpl#downloadAvailableUpdateFile(java.lang.String)}
+     * .
+     */
+    @Test
+    public void testRetrieveAvailableUpdateFile() {
+        // String availableUpdateVersion = download
+        // assertNotNull(availableUpdateVersion);
+    }
+
+    /**
+     * Test method for
+     * {@link be.jsams.server.service.update.impl.DownloaderServiceImpl#retrieveHostStringForUpdates()}
+     * .
+     * 
+     * @throws IOException
+     */
+    @Test
+    public void testRetrieveHostStringForUpdates() throws IOException {
         List<String> stringForUpdates = downloaderService.retrieveHostStringForUpdates();
         assertFalse(stringForUpdates.isEmpty());
     }
