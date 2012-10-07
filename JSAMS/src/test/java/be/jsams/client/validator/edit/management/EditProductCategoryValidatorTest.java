830659bdf4f62c19cd8cfb208a9fdd3bc6e2b271
chesteric31 <chesteric31@users.noreply.github.com>
2012-10-07 14:45:39 +0000

    Issue 17: 	Addings and corrections of JUnits test: adding of EditProductCategoryValidatorTest.

---
+++ JSAMS/src/test/java/be/jsams/client/validator/edit/management/EditProductCategoryValidatorTest.java
@@ -0,0 +1,96 @@
+package be.jsams.client.validator.edit.management;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import be.jsams.client.i18n.I18nApplicationContext;
+import be.jsams.client.i18n.JsamsI18nLabelResource;
+import be.jsams.client.i18n.JsamsI18nResource;
+import be.jsams.client.validator.edit.AbstractEditValidatorTest;
+import be.jsams.common.bean.model.MockBeanGenerator;
+import be.jsams.common.bean.model.management.ProductCategoryBean;
+
+import com.jgoodies.validation.ValidationResult;
+
+/**
+ * Test class for {@link EditProductCategoryValidator} class.
+ *
+ * @author chesteric31
+ * @version $Revision$ $Date::                  $ $Author$
+ */
+public class EditProductCategoryValidatorTest extends AbstractEditValidatorTest {
+
+    private EditProductCategoryValidator validator;
+    private ProductCategoryBean bean;
+
+    /**
+     * Setup method.
+     */
+    @Before
+    public void setUp() {
+        I18nApplicationContext.setContext(new ClassPathXmlApplicationContext(I18nApplicationContext.CONFIG));
+        validator = new EditProductCategoryValidator();
+        bean = MockBeanGenerator.generateMockProductCategory();
+    }
+    
+    /**
+     * Test method for
+     * {@link be.jsams.client.validator.edit.management.EditProductCategoryValidator
+     * #validate(be.jsams.common.bean.model.management.ProductCategoryBean)}
+     * .
+     */
+    @Test
+    public void testValidate() {
+        ValidationResult result = validator.validate(bean);
+        assertTrue(result.isEmpty());
+    }
+
+    /**
+     * Test method for
+     * {@link be.jsams.client.validator.edit.management.EditProductCategoryValidator
+     * #validate(be.jsams.common.bean.model.management.ProductCategoryBean)}
+     * .
+     */
+    @Test
+    public void testValidateWrongLabel() {
+        bean.setLabel("#");
+        ValidationResult result = validator.validate(bean);
+        String formattedText = retrieveFormattedText(result);
+        assertTrue(formattedText.contains(JsamsI18nResource.ERROR_IS_ALPHANUMERIC.getTranslation()));
+        assertTrue(formattedText.contains(JsamsI18nLabelResource.LABEL_PRODUCT_CATEGORY_EN.getTranslation()));
+    }
+
+    /**
+     * Test method for
+     * {@link be.jsams.client.validator.edit.management.EditProductCategoryValidator
+     * #validate(be.jsams.common.bean.model.management.ProductCategoryBean)}
+     * .
+     */
+    @Test
+    public void testValidateWrongLabelFr() {
+        bean.setLabelFr("#");
+        ValidationResult result = validator.validate(bean);
+        String formattedText = retrieveFormattedText(result);
+        assertTrue(formattedText.contains(JsamsI18nResource.ERROR_IS_ALPHANUMERIC.getTranslation()));
+        assertTrue(formattedText.contains(JsamsI18nLabelResource.LABEL_PRODUCT_CATEGORY_FR.getTranslation()));
+    }
+
+    /**
+     * Test method for
+     * {@link be.jsams.client.validator.edit.management.EditProductCategoryValidator
+     * #validate(be.jsams.common.bean.model.management.ProductCategoryBean)}
+     * .
+     */
+    @Test
+    public void testValidateWrongLabelNl() {
+        bean.setLabelNl("#");
+        ValidationResult result = validator.validate(bean);
+        String formattedText = retrieveFormattedText(result);
+        assertTrue(formattedText.contains(JsamsI18nResource.ERROR_IS_ALPHANUMERIC.getTranslation()));
+        assertTrue(formattedText.contains(JsamsI18nLabelResource.LABEL_PRODUCT_CATEGORY_NL.getTranslation()));
+    }
+
+}
