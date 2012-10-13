48701e3dc32bfb9ca8c9f85421d366a0dd77ab18
chesteric31 <chesteric31@a5fa9106-e037-4f10-9215-0b9fab2b6631>
2012-10-13 20:27:54 +0000

    Issue 17: 	Addings and corrections of JUnits test: adding of SearchCustomerValidatorTest adding of SearchProductValidatorTest.

---
+++ trunk/JSAMS/src/test/java/be/jsams/client/validator/search/management/SearchCustomerValidatorTest.java
@@ -0,0 +1,94 @@
+package be.jsams.client.validator.search.management;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import be.jsams.client.i18n.I18nApplicationContext;
+import be.jsams.client.i18n.JsamsI18nLabelResource;
+import be.jsams.client.i18n.JsamsI18nResource;
+import be.jsams.client.validator.AbstractValidatorTest;
+import be.jsams.common.bean.model.MockBeanGenerator;
+import be.jsams.common.bean.model.management.CustomerBean;
+
+import com.jgoodies.validation.ValidationResult;
+
+/**
+ * Test class for {@link SearchCustomerValidator} class.
+ *
+ * @author chesteric31
+ * @version $Revision$ $Date::                  $ $Author$
+ */
+public class SearchCustomerValidatorTest extends AbstractValidatorTest {
+     
+    private SearchCustomerValidator validator;
+    private CustomerBean bean;
+
+    /**
+     * Setup method.
+     */
+    @Before
+    public void setUp() {
+        I18nApplicationContext.setContext(new ClassPathXmlApplicationContext(I18nApplicationContext.CONFIG));
+        validator = new SearchCustomerValidator();
+        bean = MockBeanGenerator.generateMockCustomer();
+    }
+
+    /**
+     * Test method for {@link be.jsams.client.validator.search.management.SearchCustomerValidator
+     * #validate(be.jsams.common.bean.model.management.CustomerBean)}.
+     */
+    @Test
+    public void testValidate() {
+        ValidationResult result = validator.validate(bean);
+        assertTrue(result.isEmpty());
+    }
+
+    /**
+     * Test method for
+     * {@link be.jsams.client.validator.search.management.SearchCustomerValidator
+     * #validate(be.jsams.common.bean.model.management.CustomerBean)}
+     * .
+     */
+    @Test
+    public void testValidateWrongName() {
+        bean.setName("#");
+        ValidationResult result = validator.validate(bean);
+        String formattedText = retrieveFormattedText(result);
+        assertTrue(formattedText.contains(JsamsI18nResource.ERROR_IS_ALPHANUMERIC.getTranslation()));
+        assertTrue(formattedText.contains(JsamsI18nLabelResource.LABEL_NAME.getTranslation()));
+    }
+
+    /**
+     * Test method for
+     * {@link be.jsams.client.validator.search.management.SearchCustomerValidator
+     * #validate(be.jsams.common.bean.model.management.CustomerBean)}
+     * .
+     */
+    @Test
+    public void testValidateWrongPhone() {
+        bean.getContactInformation().setPhone("!");
+        ValidationResult result = validator.validate(bean);
+        String formattedText = retrieveFormattedText(result);
+        assertTrue(formattedText.contains(JsamsI18nResource.ERROR_IS_ALPHANUMERIC.getTranslation()));
+        assertTrue(formattedText.contains(JsamsI18nLabelResource.LABEL_PHONE.getTranslation()));
+    }
+
+    /**
+     * Test method for
+     * {@link be.jsams.client.validator.search.management.SearchCustomerValidator
+     * #validate(be.jsams.common.bean.model.management.CustomerBean)}
+     * .
+     */
+    @Test
+    public void testValidateWrongZipCode() {
+        bean.getBillingAddress().setZipCode("!");
+        ValidationResult result = validator.validate(bean);
+        String formattedText = retrieveFormattedText(result);
+        assertTrue(formattedText.contains(JsamsI18nResource.ERROR_IS_ALPHANUMERIC.getTranslation()));
+        assertTrue(formattedText.contains(JsamsI18nLabelResource.LABEL_ZIP_CODE.getTranslation()));
+    }
+
+}
---
+++ trunk/JSAMS/src/test/java/be/jsams/client/validator/search/management/SearchProductValidatorTest.java
@@ -0,0 +1,79 @@
+package be.jsams.client.validator.search.management;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import be.jsams.client.i18n.I18nApplicationContext;
+import be.jsams.client.i18n.JsamsI18nLabelResource;
+import be.jsams.client.i18n.JsamsI18nResource;
+import be.jsams.client.validator.AbstractValidatorTest;
+import be.jsams.common.bean.model.MockBeanGenerator;
+import be.jsams.common.bean.model.management.ProductBean;
+
+import com.jgoodies.validation.ValidationResult;
+
+/**
+ * Test class for {@link SearchProductValidator} class.
+ *
+ * @author chesteric31
+ * @version $Revision$ $Date::                  $ $Author$
+ */
+public class SearchProductValidatorTest extends AbstractValidatorTest {
+     
+    private SearchProductValidator validator;
+    private ProductBean bean;
+
+    /**
+     * Setup method.
+     */
+    @Before
+    public void setUp() {
+        I18nApplicationContext.setContext(new ClassPathXmlApplicationContext(I18nApplicationContext.CONFIG));
+        validator = new SearchProductValidator();
+        bean = MockBeanGenerator.generateMockProduct();
+    }
+
+    /**
+     * Test method for {@link be.jsams.client.validator.search.management.SearchProductValidator
+     * #validate(be.jsams.common.bean.model.management.ProductBean)}.
+     */
+    @Test
+    public void testValidate() {
+        ValidationResult result = validator.validate(bean);
+        assertTrue(result.isEmpty());
+    }
+
+    /**
+     * Test method for
+     * {@link be.jsams.client.validator.search.management.SearchProductValidator
+     * #validate(be.jsams.common.bean.model.management.ProductBean)}
+     * .
+     */
+    @Test
+    public void testValidateWrongName() {
+        bean.setName("#");
+        ValidationResult result = validator.validate(bean);
+        String formattedText = retrieveFormattedText(result);
+        assertTrue(formattedText.contains(JsamsI18nResource.ERROR_IS_ALPHANUMERIC.getTranslation()));
+        assertTrue(formattedText.contains(JsamsI18nLabelResource.LABEL_PRODUCT_LABEL.getTranslation()));
+    }
+
+    /**
+     * Test method for
+     * {@link be.jsams.client.validator.search.management.SearchProductValidator
+     * #validate(be.jsams.common.bean.model.management.ProductBean)}
+     * .
+     */
+    @Test
+    public void testValidateBlankCtegory() {
+        bean.setCategory(null);
+        ValidationResult result = validator.validate(bean);
+        String formattedText = retrieveFormattedText(result);
+        assertTrue(formattedText.contains(JsamsI18nResource.ERROR_IS_MANDATORY.getTranslation()));
+        assertTrue(formattedText.contains(JsamsI18nLabelResource.LABEL_PRODUCT_CATEGORY.getTranslation()));
+    }
+
+}
