b0a965408f57da93c2f9882c2bbf5368805c8d33
chesteric31 <chesteric31@a5fa9106-e037-4f10-9215-0b9fab2b6631>
2012-10-14 12:45:35 +0000

    Issue 17: 	Addings and corrections of JUnits test: adding of DetailsValidatorTest.

---
+++ trunk/JSAMS/src/test/java/be/jsams/client/validator/wizard/DetailsValidatorTest.java
@@ -0,0 +1,65 @@
+package be.jsams.client.validator.wizard;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import be.jsams.client.i18n.I18nApplicationContext;
+import be.jsams.client.i18n.JsamsI18nLabelResource;
+import be.jsams.client.i18n.JsamsI18nResource;
+import be.jsams.client.validator.AbstractValidatorTest;
+import be.jsams.common.bean.model.MockBeanGenerator;
+import be.jsams.common.bean.model.transfer.TransferBean;
+
+import com.jgoodies.validation.ValidationResult;
+
+/**
+ * Test class for {@link DetailsValidator} class.
+ *
+ * @author chesteric31
+ * @version $Revision$ $Date::                  $ $Author$
+ */
+public class DetailsValidatorTest extends AbstractValidatorTest {
+
+    private DetailsValidator validator;
+    private TransferBean bean;
+
+    /**
+     * Setup method.
+     */
+    @Before
+    public void setUp() {
+        I18nApplicationContext.setContext(new ClassPathXmlApplicationContext(I18nApplicationContext.CONFIG));
+        validator = new DetailsValidator();
+        bean = MockBeanGenerator.generateMockTransfer();
+    }
+
+    /**
+     * Test method for {@link be.jsams.client.validator.wizard.DetailsValidator
+     * #validate(be.jsams.common.bean.model.transfer.TransferBean)}.
+     */
+    @Test
+    public void testValidate() {
+        ValidationResult result = validator.validate(bean);
+        assertTrue(result.isEmpty());
+    }
+
+    /**
+     * Test method for {@link be.jsams.client.validator.wizard.DetailsValidator
+     * #validate(be.jsams.common.bean.model.transfer.TransferBean)}.
+     */
+    @Test
+    public void testValidateBlankDocuments() {
+        bean.setEstimateDetails(null);
+        bean.setCommandDetails(null);
+        bean.setDeliveryOrderDetails(null);
+        bean.setBillDetails(null);
+        ValidationResult result = validator.validate(bean);
+        String formattedText = retrieveFormattedText(result);
+        assertTrue(formattedText.contains(JsamsI18nResource.ERROR_IS_MANDATORY_TO_SELECT.getTranslation()));
+        assertTrue(formattedText.contains(JsamsI18nLabelResource.LABEL_DOCUMENT_DETAILS.getTranslation()));
+    }
+
+}
