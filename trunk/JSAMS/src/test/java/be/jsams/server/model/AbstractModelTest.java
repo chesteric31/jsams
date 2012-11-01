a16a8a1585b0711d3bde7f514e84b9d7223f424b
chesteric31 <chesteric31@a5fa9106-e037-4f10-9215-0b9fab2b6631>
2012-11-01 14:02:44 +0000

    Issue 17: 	Addings and corrections of JUnits test: adding of EstimateTest and CommandTest.

--- trunk/JSAMS/src/test/java/be/jsams/server/model/AbstractModelTest.java
+++ trunk/JSAMS/src/test/java/be/jsams/server/model/AbstractModelTest.java
@@ -8,12 +8,18 @@ import be.jsams.common.bean.model.ContactInformationBean;
 import be.jsams.common.bean.model.LegalFormBean;
 import be.jsams.common.bean.model.PaymentModeBean;
 import be.jsams.common.bean.model.SocietyBean;
+import be.jsams.common.bean.model.management.AgentBean;
+import be.jsams.common.bean.model.management.CustomerBean;
+import be.jsams.common.bean.model.management.ProductBean;
 import be.jsams.common.bean.model.management.ProductCategoryBean;
+import be.jsams.server.model.management.Agent;
+import be.jsams.server.model.management.Customer;
+import be.jsams.server.model.management.Product;
 import be.jsams.server.model.management.ProductCategory;
 
 /**
  * 
- *
+ * 
  * @author chesteric31
  * @version $Revision$ $Date::                  $ $Author$
  */
@@ -22,149 +28,246 @@ public abstract class AbstractModelTest {
     /**
      * Checks if the data are equals.
      * 
-     * @param societyBean the {@link SocietyBean} to check
-     * @param society the {@link Society} to use
+     * @param bean the {@link ProductBean} to use
+     * @param product the {@link Product} to check
      */
-    protected void checkSociety(SocietyBean societyBean, Society society) {
-        String activity = society.getActivity();
-        assertTrue(activity.equals(societyBean.getActivity()));
-        String name = society.getName();
-        assertEquals(name, societyBean.getName());
-        String responsible = society.getResponsible();
-        assertEquals(responsible, societyBean.getResponsible());
-        Address address = society.getAddress();
-        AddressBean addressBean = societyBean.getAddress();
-        checkAddress(addressBean, address);
-        Double capital = society.getCapital();
-        assertEquals(capital, societyBean.getCapital());
-        ContactInformation contactInformation = society.getContactInformation();
-        ContactInformationBean contactInformationBean = societyBean.getContactInformation();
-        checkContactInformation(contactInformationBean, contactInformation);
-        LegalForm legalForm = society.getLegalForm();
-        LegalFormBean legalFormBean = societyBean.getLegalForm();
-        checkLegalForm(legalFormBean, legalForm);
-        byte[] logo = society.getLogo();
-        assertEquals(logo, societyBean.getLogo());
-        String vatNumber = society.getVatNumber();
-        assertEquals(vatNumber, societyBean.getVatNumber());
-        Long id = society.getId();
-        assertEquals(id, societyBean.getId());
-
+    protected void checkProduct(ProductBean bean, Product product) {
+        Double price = product.getPrice();
+        assertTrue(price.equals(bean.getPrice()));
+        String name = product.getName();
+        assertEquals(name, bean.getName());
+        ProductCategory category = product.getCategory();
+        ProductCategoryBean categoryBean = bean.getCategory();
+        checkProductCategory(categoryBean, category);
+        int quantityStock = product.getQuantityStock();
+        assertEquals(quantityStock, bean.getQuantityStock());
+        int reorderLevel = product.getReorderLevel();
+        assertEquals(reorderLevel, bean.getReorderLevel());
+        Double vatApplicable = product.getVatApplicable();
+        assertEquals(vatApplicable, bean.getVatApplicable());
+        Long id = product.getId();
+        assertEquals(id, bean.getId());
     }
     
     /**
      * Checks if the data are equals.
      * 
-     * @param addressBean the {@link AddressBean} to check
-     * @param address the {@link Address} to use
+     * @param bean the {@link CustomerBean} to use
+     * @param customer the {@link Customer} to check
      */
-    protected void checkAddress(AddressBean addressBean, Address address) {
+    protected void checkCustomer(CustomerBean bean, Customer customer) {
+        String bank1 = customer.getBank1();
+        assertTrue(bank1.equals(bean.getBank1()));
+        String bank2 = customer.getBank2();
+        assertTrue(bank2.equals(bean.getBank2()));
+        String description = customer.getDescription();
+        assertTrue(description.equals(bean.getDescription()));
+        String firstName = customer.getFirstName();
+        assertTrue(firstName.equals(bean.getFirstName()));
+        String name = customer.getName();
+        assertEquals(name, bean.getName());
+        Double creditLimit = customer.getCreditLimit();
+        assertEquals(creditLimit, bean.getCreditLimit());
+        Double defaultDiscountRate = customer.getDefaultDiscountRate();
+        assertEquals(defaultDiscountRate, bean.getDefaultDiscountRate());
+        Double vatApplicable = customer.getVatApplicable();
+        assertEquals(vatApplicable, bean.getVatApplicable());
+        String vatNumber = customer.getVatNumber();
+        assertEquals(vatNumber, bean.getVatNumber());
+        Address billingAddress = customer.getBillingAddress();
+        AddressBean billingAddressBean = bean.getBillingAddress();
+        checkAddress(billingAddressBean, billingAddress);
+        Address deliveryAddress = customer.getDeliveryAddress();
+        AddressBean deliveryAddressBean = bean.getDeliveryAddress();
+        checkAddress(deliveryAddressBean, deliveryAddress);
+        ContactInformation contactInformation = customer.getContactInformation();
+        ContactInformationBean contactInformationBean = bean.getContactInformation();
+        checkContactInformation(contactInformationBean, contactInformation);
+        LegalForm legalForm = customer.getLegalForm();
+        checkLegalForm(bean.getLegalForm(), legalForm);
+        PaymentMode paymentMode = customer.getPaymentMode();
+        checkPaymentMode(bean.getPaymentMode(), paymentMode);
+        Civility civility = customer.getCivility();
+        CivilityBean civilityBean = bean.getCivility();
+        checkCivility(civilityBean, civility);
+        Society society = customer.getSociety();
+        SocietyBean societyBean = bean.getSociety();
+        checkSociety(societyBean, society);
+        Long id = customer.getId();
+        assertEquals(id, bean.getId());
+    }
+
+    /**
+     * Checks if the data are equals.
+     * 
+     * @param bean the {@link AgentBean} to use
+     * @param agent the {@link Agent} to check
+     */
+    protected void checkAgent(AgentBean bean, Agent agent) {
+        String function = agent.getFunction();
+        assertTrue(function.equals(bean.getFunction()));
+        String name = agent.getName();
+        assertEquals(name, bean.getName());
+        Address address = agent.getAddress();
+        AddressBean addressBean = bean.getAddress();
+        checkAddress(addressBean, address);
+        ContactInformation contactInformation = agent.getContactInformation();
+        ContactInformationBean contactInformationBean = bean.getContactInformation();
+        checkContactInformation(contactInformationBean, contactInformation);
+        Civility civility = agent.getCivility();
+        CivilityBean civilityBean = bean.getCivility();
+        checkCivility(civilityBean, civility);
+        Society society = agent.getSociety();
+        SocietyBean societyBean = bean.getSociety();
+        checkSociety(societyBean, society);
+        Long id = agent.getId();
+        assertEquals(id, bean.getId());
+    }
+
+    /**
+     * Checks if the data are equals.
+     * 
+     * @param bean the {@link SocietyBean} to use
+     * @param society the {@link Society} to check
+     */
+    protected void checkSociety(SocietyBean bean, Society society) {
+        String activity = society.getActivity();
+        assertTrue(activity.equals(bean.getActivity()));
+        String name = society.getName();
+        assertEquals(name, bean.getName());
+        String responsible = society.getResponsible();
+        assertEquals(responsible, bean.getResponsible());
+        Address address = society.getAddress();
+        AddressBean addressBean = bean.getAddress();
+        checkAddress(addressBean, address);
+        Double capital = society.getCapital();
+        assertEquals(capital, bean.getCapital());
+        ContactInformation contactInformation = society.getContactInformation();
+        ContactInformationBean contactInformationBean = bean.getContactInformation();
+        checkContactInformation(contactInformationBean, contactInformation);
+        LegalForm legalForm = society.getLegalForm();
+        LegalFormBean legalFormBean = bean.getLegalForm();
+        checkLegalForm(legalFormBean, legalForm);
+        byte[] logo = society.getLogo();
+        assertEquals(logo, bean.getLogo());
+        String vatNumber = society.getVatNumber();
+        assertEquals(vatNumber, bean.getVatNumber());
+        Long id = society.getId();
+        assertEquals(id, bean.getId());
+    }
+
+    /**
+     * Checks if the data are equals.
+     * 
+     * @param bean the {@link AddressBean} to use
+     * @param address the {@link Address} to check
+     */
+    protected void checkAddress(AddressBean bean, Address address) {
         String box = address.getBox();
-        assertTrue(box.equals(addressBean.getBox()));
+        assertTrue(box.equals(bean.getBox()));
         String city = address.getCity();
-        assertEquals(city, addressBean.getCity());
+        assertEquals(city, bean.getCity());
         String country = address.getCountry();
-        assertEquals(country, addressBean.getCountry());
+        assertEquals(country, bean.getCountry());
         String number = address.getNumber();
-        assertEquals(number, addressBean.getNumber());
+        assertEquals(number, bean.getNumber());
         String street = address.getStreet();
-        assertEquals(street, addressBean.getStreet());
+        assertEquals(street, bean.getStreet());
         String zipCode = address.getZipCode();
-        assertEquals(zipCode, addressBean.getZipCode());
+        assertEquals(zipCode, bean.getZipCode());
         Long id = address.getId();
-        assertEquals(id, addressBean.getId());
+        assertEquals(id, bean.getId());
     }
 
     /**
      * Checks if the data are equals.
      * 
-     * @param contactInformationBean the {@link ContactInformationBean} to check
-     * @param contactInformation the {@link ContactInformation} to use
+     * @param bean the {@link ContactInformationBean} to use
+     * @param contactInformation the {@link ContactInformation} to check
      */
-    protected void checkContactInformation(ContactInformationBean contactInformationBean,
-            ContactInformation contactInformation) {
+    protected void checkContactInformation(ContactInformationBean bean, ContactInformation contactInformation) {
         String email = contactInformation.getEmail();
-        assertTrue(email.equals(contactInformationBean.getEmail()));
+        assertTrue(email.equals(bean.getEmail()));
         String fax = contactInformation.getFax();
-        assertEquals(fax, contactInformationBean.getFax());
+        assertEquals(fax, bean.getFax());
         String mobile = contactInformation.getMobile();
-        assertEquals(mobile, contactInformationBean.getMobile());
+        assertEquals(mobile, bean.getMobile());
         String phone = contactInformation.getPhone();
-        assertEquals(phone, contactInformationBean.getPhone());
+        assertEquals(phone, bean.getPhone());
         String website = contactInformation.getWebsite();
-        assertEquals(website, contactInformationBean.getWebsite());
+        assertEquals(website, bean.getWebsite());
         Long id = contactInformation.getId();
-        assertEquals(id, contactInformationBean.getId());
+        assertEquals(id, bean.getId());
     }
 
     /**
      * Checks if the data are equals.
      * 
-     * @param legalFormBean the {@link LegalFormBean} to check
-     * @param legalForm the {@link LegalForm} to use
+     * @param bean the {@link LegalFormBean} to use
+     * @param legalForm the {@link LegalForm} to check
      */
-    protected void checkLegalForm(LegalFormBean legalFormBean, LegalForm legalForm) {
+    protected void checkLegalForm(LegalFormBean bean, LegalForm legalForm) {
         String label = legalForm.getLabel();
-        assertTrue(label.equals(legalFormBean.getLabel()));
+        assertTrue(label.equals(bean.getLabel()));
         String labelFr = legalForm.getLabelFr();
-        assertTrue(labelFr.equals(legalFormBean.getLabelFr()));
+        assertTrue(labelFr.equals(bean.getLabelFr()));
         String labelNl = legalForm.getLabelNl();
-        assertTrue(labelNl.equals(legalFormBean.getLabelNl()));
+        assertTrue(labelNl.equals(bean.getLabelNl()));
         Long id = legalForm.getId();
-        assertEquals(id, legalFormBean.getId());
+        assertEquals(id, bean.getId());
     }
 
     /**
      * Checks if the data are equals.
      * 
-     * @param civilityBean the {@link CivilityBean} to check
-     * @param civility the {@link Civility} to use
+     * @param bean the {@link CivilityBean} to use
+     * @param civility the {@link Civility} to check
      */
-    protected void checkCivility(CivilityBean civilityBean, Civility civility) {
+    protected void checkCivility(CivilityBean bean, Civility civility) {
         String label = civility.getLabel();
-        assertTrue(label.equals(civilityBean.getLabel()));
+        assertTrue(label.equals(bean.getLabel()));
         String labelFr = civility.getLabelFr();
-        assertTrue(labelFr.equals(civilityBean.getLabelFr()));
+        assertTrue(labelFr.equals(bean.getLabelFr()));
         String labelNl = civility.getLabelNl();
-        assertTrue(labelNl.equals(civilityBean.getLabelNl()));
+        assertTrue(labelNl.equals(bean.getLabelNl()));
         Long id = civility.getId();
-        assertEquals(id, civilityBean.getId());
+        assertEquals(id, bean.getId());
     }
 
     /**
      * Checks if the data are equals.
      * 
-     * @param paymentModeBean the {@link PaymentModeBean} to check
-     * @param paymentMode the {@link PaymentMode} to use
+     * @param bean the {@link PaymentModeBean} to use
+     * @param paymentMode the {@link PaymentMode} to check
      */
-    protected void checkPaymentMode(PaymentModeBean paymentModeBean, PaymentMode paymentMode) {
+    protected void checkPaymentMode(PaymentModeBean bean, PaymentMode paymentMode) {
         String label = paymentMode.getLabel();
-        assertTrue(label.equals(paymentModeBean.getLabel()));
+        assertTrue(label.equals(bean.getLabel()));
         String labelFr = paymentMode.getLabelFr();
-        assertTrue(labelFr.equals(paymentModeBean.getLabelFr()));
+        assertTrue(labelFr.equals(bean.getLabelFr()));
         String labelNl = paymentMode.getLabelNl();
-        assertTrue(labelNl.equals(paymentModeBean.getLabelNl()));
+        assertTrue(labelNl.equals(bean.getLabelNl()));
         Long id = paymentMode.getId();
-        assertEquals(id, paymentModeBean.getId());
+        assertEquals(id, bean.getId());
     }
 
     /**
      * Checks if the data are equals.
      * 
-     * @param productCategoryBean the {@link ProductCategoryBean} to check
-     * @param productCategory the {@link ProductCategory} to use
+     * @param bean the {@link ProductCategoryBean} to use
+     * @param productCategory the {@link ProductCategory} to check
      */
-    protected void checkProductCategory(ProductCategoryBean productCategoryBean, ProductCategory productCategory) {
+    protected void checkProductCategory(ProductCategoryBean bean, ProductCategory productCategory) {
         String label = productCategory.getLabel();
-        assertTrue(label.equals(productCategoryBean.getLabel()));
+        assertTrue(label.equals(bean.getLabel()));
         String labelFr = productCategory.getLabelFr();
-        assertTrue(labelFr.equals(productCategoryBean.getLabelFr()));
+        assertTrue(labelFr.equals(bean.getLabelFr()));
         String labelNl = productCategory.getLabelNl();
-        assertTrue(labelNl.equals(productCategoryBean.getLabelNl()));
+        assertTrue(labelNl.equals(bean.getLabelNl()));
         Society society = productCategory.getSociety();
-        checkSociety(productCategoryBean.getSociety(), society);
+        checkSociety(bean.getSociety(), society);
         Long id = productCategory.getId();
-        assertEquals(id, productCategoryBean.getId());
+        assertEquals(id, bean.getId());
     }
 
 }
---
+++ trunk/JSAMS/src/test/java/be/jsams/server/model/sale/EstimateTest.java
@@ -0,0 +1,59 @@
+package be.jsams.server.model.sale;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import be.jsams.common.bean.model.MockBeanGenerator;
+import be.jsams.common.bean.model.sale.EstimateBean;
+import be.jsams.common.bean.model.sale.detail.EstimateDetailBean;
+import be.jsams.server.model.AbstractModelTest;
+import be.jsams.server.model.sale.detail.EstimateDetail;
+
+/**
+ * Test class for {@link Estimate} class.
+ * 
+ * @author chesteric31
+ * @version $Revision$ $Date::                  $ $Author$
+ */
+public class EstimateTest extends AbstractModelTest {
+
+    private Estimate estimate;
+
+    /**
+     * Test method for
+     * {@link be.jsams.server.model.sale.Estimate#Estimate(be.jsams.common.bean.model.sale.EstimateBean)}
+     * .
+     */
+    @Test
+    public void testEstimateEstimateBean() {
+        EstimateBean bean = MockBeanGenerator.generateMockEstimate();
+        estimate = new Estimate(bean);
+        checkAgent(bean.getAgent(), estimate.getAgent());
+        checkAddress(bean.getBillingAddress(), estimate.getBillingAddress());
+        assertEquals(bean.getCreationDate(), estimate.getCreationDate());
+        checkCustomer(bean.getCustomer(), estimate.getCustomer());
+        assertEquals(bean.getDiscountRate(), estimate.getDiscountRate());
+        assertEquals(bean.isTransferred(), estimate.isTransferred());
+        assertEquals(bean.getRemark(), estimate.getRemark());
+        List<EstimateDetail> details = estimate.getDetails();
+        List<EstimateDetailBean> detailsBean = bean.getDetails();
+        if (details != null && !details.isEmpty()) {
+            int i = 0;
+            for (EstimateDetail detail : details) {
+                EstimateDetailBean detailBean = detailsBean.get(i);
+                assertEquals(detailBean.getDiscountRate(), detail.getDiscountRate());
+                assertEquals(detailBean.getId(), detail.getId());
+                assertEquals(detailBean.getPrice(), detail.getPrice());
+                checkProduct(detailBean.getProduct(), detail.getProduct());
+                assertEquals(detailBean.getQuantity(), detail.getQuantity());
+                assertEquals(detailBean.getVatApplicable(), detail.getVatApplicable());
+                assertEquals(detailBean.isTransferred(), detail.isTransferred());
+                i++;
+            }
+        }
+    }
+
+}
