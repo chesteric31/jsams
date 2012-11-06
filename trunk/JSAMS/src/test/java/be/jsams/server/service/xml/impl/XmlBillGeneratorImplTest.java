c632e07ebb0e9b9544d7fbbb86204a5a218f9f0e
chesteric31 <chesteric31@a5fa9106-e037-4f10-9215-0b9fab2b6631>
2012-11-06 13:28:26 +0000

    Issue 17: 	Addings and corrections of JUnits test: adding of XmlBillGeneratorImplTest and XmlCreditNoteGeneratorImplTest.

---
+++ trunk/JSAMS/src/test/java/be/jsams/server/service/xml/impl/XmlBillGeneratorImplTest.java
@@ -0,0 +1,58 @@
+package be.jsams.server.service.xml.impl;
+
+import static org.junit.Assert.assertEquals;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.List;
+
+import org.junit.Test;
+
+import be.jsams.common.bean.model.MockBeanGenerator;
+import be.jsams.common.bean.model.sale.BillBean;
+import be.jsams.common.bean.model.sale.detail.BillDetailBean;
+import be.jsams.server.model.xml.bill.BillXml;
+import be.jsams.server.model.xml.bill.DetailXml;
+import be.jsams.server.model.xml.bill.DetailsXml;
+
+/**
+ * 
+ *
+ * @author chesteric31
+ * @version $Revision$ $Date::                  $ $Author$
+ */
+public class XmlBillGeneratorImplTest extends AbstractXmlTest {
+
+    private XmlBillGeneratorImpl generator = new XmlBillGeneratorImpl();
+    
+    /**
+     * Test method for
+     * {@link be.jsams.server.service.xml.impl.XmlBillGeneratorImpl
+     * #generateXml(be.jsams.common.bean.model.sale.BillBean)}
+     * .
+     */
+    @Test
+    public void testGenerateXml() {
+        BillBean bean = MockBeanGenerator.generateMockBill();
+        BillXml xml = generator.generateXml(bean);
+        checkAddress(bean.getBillingAddress(), xml.getAddress());
+        assertEquals(bean.getCreationDate(), xml.getCreationDate());
+        checkCustomer(bean.getCustomer(), xml.getCustomer());
+        assertEquals(bean.getDueDate(), xml.getDueDate());
+        DetailsXml detailsXml = xml.getDetails();
+        List<BillDetailBean> detailsBean = bean.getDetails();
+        List<DetailXml> detail = detailsXml.getDetail();
+        if (detail != null && !detail.isEmpty()) {
+            int i = 0;
+            for (DetailXml detailXml : detail) {
+                BillDetailBean detailBean = detailsBean.get(i);
+                checkProduct(detailBean.getProduct(), detailXml.getProduct());
+                assertEquals(BigDecimal.valueOf(detailBean.getPrice()), detailXml.getPrice());
+                assertEquals(BigInteger.valueOf(detailBean.getQuantity()), detailXml.getQuantity());
+            }
+        }
+        assertEquals(bean.getId(), xml.getNumber());
+        checkSociety(bean.getSociety(), xml.getSociety());
+    }
+
+}
