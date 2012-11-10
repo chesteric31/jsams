7d72aea88074c73a73292aee23ba06c711fdca90
chesteric31 <chesteric31@users.noreply.github.com>
2012-11-10 19:21:32 +0000

    Bug fix for alternate color in table for boolean check box.

--- JSAMS/src/main/java/be/jsams/client/renderer/JsamsBooleanTableCellRenderer.java
+++ JSAMS/src/main/java/be/jsams/client/renderer/JsamsBooleanTableCellRenderer.java
@@ -46,9 +46,9 @@ public class JsamsBooleanTableCellRenderer extends JCheckBox implements TableCel
             setBackground(table.getSelectionBackground());
         } else {
             if (row % 2 == 0) {
-                setBackground(UIManager.getColor("Table.alternateRowColor"));
-            } else {
                 setBackground(Color.WHITE);
+            } else {
+                setBackground(UIManager.getColor("Table.alternateRowColor"));
             }
         }
 
