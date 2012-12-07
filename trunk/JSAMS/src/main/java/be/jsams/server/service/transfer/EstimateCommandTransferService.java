5c2ae8bfb97be674c6e2ced759dcd315036e068f
chesteric31 <chesteric31@a5fa9106-e037-4f10-9215-0b9fab2b6631>
2012-12-07 17:40:14 +0000

    Javadoc.

--- trunk/JSAMS/src/main/java/be/jsams/server/service/transfer/EstimateCommandTransferService.java
+++ trunk/JSAMS/src/main/java/be/jsams/server/service/transfer/EstimateCommandTransferService.java
@@ -1,8 +1,19 @@
 package be.jsams.server.service.transfer;
 
 /**
+ * Marking interface for transfer of estimate to command.
+ * <p>
+ * This interface is necessary to avoid (while application starting) this sort
+ * of issue:
+ * <p>
+ * Caused by: java.lang.IllegalStateException: Cannot convert value of type
+ * [$Proxy28 implementing be.jsams.server.service.transfer.TransferService, org
+ * .springframework.aop.SpringProxy,org.springframework.aop.framework.Advised]
+ * to required type
+ * [be.jsams.server.service.transfer.EstimateCommandTransferService] for
+ * property 'estimateCommandTransferService': no matching editors or conversion
+ * strategy found
  * 
- *
  * @author chesteric31
  * @version $Revision$ $Date::                  $ $Author$
  */
