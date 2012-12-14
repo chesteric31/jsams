27fe8d1f5d88f86d01d74e8d05c5f3a8ce36ccfe
chesteric31 <chesteric31@users.noreply.github.com>
2012-12-14 08:46:15 +0000

    Refactoring single selection table into wizard.

---
+++ JSAMS/src/main/java/be/jsams/client/swing/listener/wizard/selection/single/BillWizardSingleSelectionTableML.java
@@ -0,0 +1,39 @@
+package be.jsams.client.swing.listener.wizard.selection.single;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import be.jsams.client.model.table.sale.BillTableModel;
+import be.jsams.client.swing.component.JsamsTable;
+import be.jsams.common.bean.model.sale.BillBean;
+import be.jsams.common.bean.model.transfer.TransferBean;
+
+/**
+ * Bill wizard single selection table.
+ *
+ * @author chesteric31
+ * @version $Revision$ $Date::                  $ $Author$
+ */
+public class BillWizardSingleSelectionTableML extends AbstractWizardSingleSelectionTableML {
+
+    /**
+     * Constructor.
+     * 
+     * @param bean the {@link TransferBean}
+     */
+    public BillWizardSingleSelectionTableML(TransferBean bean) {
+        super(bean);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void handleSingleClick(JsamsTable table, int selectedRowModel) {
+        BillTableModel model = (BillTableModel) table.getModel();
+        List<BillBean> beans = new ArrayList<BillBean>();
+        beans.add(model.getRow(selectedRowModel));
+        getBean().setDocuments(beans);
+    }
+
+}
---
+++ JSAMS/src/main/java/be/jsams/client/swing/listener/wizard/selection/single/DeliveryOrderWizardSingleSelectionTableML.java
@@ -0,0 +1,39 @@
+package be.jsams.client.swing.listener.wizard.selection.single;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import be.jsams.client.model.table.sale.DeliveryOrderTableModel;
+import be.jsams.client.swing.component.JsamsTable;
+import be.jsams.common.bean.model.sale.DeliveryOrderBean;
+import be.jsams.common.bean.model.transfer.TransferBean;
+
+/**
+ * Delivery order wizard single selection table.
+ * 
+ * @author chesteric31
+ * @version $Revision$ $Date::                  $ $Author$
+ */
+public class DeliveryOrderWizardSingleSelectionTableML extends AbstractWizardSingleSelectionTableML {
+
+    /**
+     * Constructor.
+     * 
+     * @param bean the {@link TransferBean}
+     */
+    public DeliveryOrderWizardSingleSelectionTableML(TransferBean bean) {
+        super(bean);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected void handleSingleClick(JsamsTable table, int selectedRowModel) {
+        DeliveryOrderTableModel model = (DeliveryOrderTableModel) table.getModel();
+        List<DeliveryOrderBean> beans = new ArrayList<DeliveryOrderBean>();
+        beans.add(model.getRow(selectedRowModel));
+        getBean().setDocuments(beans);
+    }
+
+}
