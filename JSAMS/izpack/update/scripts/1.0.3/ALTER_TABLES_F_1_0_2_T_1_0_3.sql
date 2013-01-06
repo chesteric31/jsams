9eb028401629690b25e2086c5b9a0fac9bde36ba
chesteric31 <chesteric31@users.noreply.github.com>
2013-01-06 17:19:46 +0000

    Prepare release 1.0.3

--- JSAMS/izpack/update/scripts/1.0.3/ALTER_TABLES_F_1_0_2_T_1_0_3.sql
+++ JSAMS/izpack/update/scripts/1.0.3/ALTER_TABLES_F_1_0_2_T_1_0_3.sql
@@ -1,2 +1,12 @@
 ALTER TABLE BTS.BILL DROP COLUMN PAID;
 ALTER TABLE BTS.BILL ADD PAYMENT_DATE DATE;
+
+UPDATE BTS.LEGAL_FORM SET LABEL_FR="Société Privée à Responsabilité Limitée (Unipersonnelle) (SPRL)" WHERE ID = 2;
+UPDATE BTS.LEGAL_FORM SET LABEL_FR="Société Anonyme (SA)" WHERE ID = 4;
+UPDATE BTS.LEGAL_FORM SET LABEL_FR="Société en Commandité par Actions (SCA)" WHERE ID = 5;
+UPDATE BTS.LEGAL_FORM SET LABEL_FR="Société Coopérative à Responsabilité Limitée (SCRL)" WHERE ID = 6;
+UPDATE BTS.LEGAL_FORM SET LABEL_NL="Coöperatieve Vennootschap met Beperkte Aansprakelijkheid (CVBA)" WHERE ID = 6;
+UPDATE BTS.LEGAL_FORM SET LABEL_FR="Société Coopérative à Responsabilité Illimitée (SCRI)" WHERE ID = 7;
+UPDATE BTS.LEGAL_FORM SET LABEL_NL="Coöperatieve Vennootschap met Onbeperkte en Hoofdelijke Aansprakelijkheid (CVOA)" WHERE ID = 7;
+UPDATE BTS.LEGAL_FORM SET LABEL_FR="Société en Nom Collectif (SNC)" WHERE ID = 8;
+UPDATE BTS.LEGAL_FORM SET LABEL_FR="Société en Commandité Simple (SCS)" WHERE ID = 9;
--- JSAMS/izpack/update/win/1.0.2/update_db_scripts.bat
+++ JSAMS/izpack/update/win/1.0.2/update_db_scripts.bat
@@ -1,14 +1,3 @@
 @ECHO OFF
-echo "stop the mysql service"
-Rem for version 1.0.2 and after it must be with win between $INSTALL_PATH and mysql
-$INSTALL_PATH\mysql-5.1.60-win32\bin\mysqld.exe" --uninstall
-net stop MySQL
-sc delete MySQL
-echo "move mysql dir to new dir: win\mysql"
-move "$INSTALL_PATH\mysql-5.1.60-win32" "$INSTALL_PATH\win\mysql-5.1.60-win32"
-$INSTALL_PATH\win\mysql-5.1.60-win32\bin\mysqld.exe" --install
-net stop MySQL
-"$INSTALL_PATH\win\mysql-5.1.60-win32\bin\mysql.exe" --user=root --password=$DB_PASSWORD --database=$DB_SCHEMA < "$INSTALL_PATH\scripts\1.0.2\ALTER_TABLES_F_1_0_1_T_1_0_2.sql"
-echo "delete all 1.0.1 files"
-del "$INSTALL_PATH\script.bat"
-del "$INSTALL_PATH\JSAMS-1.0.1-SNAPSHOT.jar"
+"$INSTALL_PATH\win\mysql-5.1.60-win32\bin\mysql.exe" --user=root --password=$DB_PASSWORD --database=$DB_SCHEMA < "$INSTALL_PATH\scripts\1.0.3\ALTER_TABLES_F_1_0_2_T_1_0_3.sql" --default-character-set=utf8
+
